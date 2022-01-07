a659e05ff49039d20b1870aff5e464fff16069c5
mmistroni <mmistroni@gmail.com>
2022-01-07 06:49:53 +0000

    adding next exercise

---
+++ src/cyclic_rotation.py
@@ -0,0 +1,11 @@
+
+def sol(A, K):
+
+    with_idx = [(item, idx) for idx, item in enumerate(A)]
+
+    mapped = [(tpl[0], (tpl[1] + K) % len(A) ) for tpl in with_idx]
+
+    sorted_arr = sorted(mapped, key=lambda tpl: tpl[1])
+    return [tpl[0] for tpl in sorted_arr]
+
+
---
+++ test/cyclic_rotation_test.py
@@ -0,0 +1,27 @@
+import unittest
+
+from Codility.src.cyclic_rotation import  sol
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        A = [3, 8, 9, 7, 6]
+        K = 3
+        res = sol(A, K)
+        self.assertEqual([9, 7, 6, 3, 8], res)  # add assertion here
+
+
+    def test_something2(self):
+        A = [0,0,0]
+        K = 1
+        res = sol(A, K)
+        self.assertEqual([0,0,0], res)  # add assertion here
+
+    def test_something3(self):
+        A = [1, 2, 3, 4]
+        K = 4
+        res = sol(A, K)
+        self.assertEqual([1, 2, 3, 4], res)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
