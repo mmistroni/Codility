b09ecb40352063899d6c83639043e877376ca2d7
mmistroni <mmistroni@gmail.com>
2022-02-01 20:18:41 +0000

    addign min avg sums

--- src/min_avg_two_sums.py
+++ src/min_avg_two_sums.py
@@ -5,6 +5,6 @@ from prefix_sums import prefix_sums
 def solution(A):
     new_array = prefix_sums(A)
 
-    all_sums = [new_array[i]/i for i in  range(0, new_array)]
-    return all_sums
+    all_sums = [new_array[i]/i+1 for i in  range(1, len(new_array))]
+    return [tpl for tpl in zip(new_array, all_sums)]
 
--- test/test_min_avg_two_sums.py
+++ test/test_min_avg_two_sums.py
@@ -3,7 +3,9 @@ import unittest
 from src.min_avg_two_sums import solution
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        A = [4, 2, 2, 5, 1, 5, 8]
+
+        print(solution(A))
 
 
 if __name__ == '__main__':
