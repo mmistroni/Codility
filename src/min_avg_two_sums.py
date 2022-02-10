0d2a00b1ec0009bb78499fd77034256d9471b3dd
mmistroni <mmistroni@gmail.com>
2022-02-10 20:34:26 +0000

    first run at prefix sums

--- src/min_avg_two_sums.py
+++ src/min_avg_two_sums.py
@@ -5,6 +5,23 @@ from prefix_sums import prefix_sums
 def solution(A):
     new_array = prefix_sums(A)
 
-    all_sums = [new_array[i]/i+1 for i in  range(1, len(new_array))]
-    return [tpl for tpl in zip(new_array, all_sums)]
+
+    # Sample problem here https://app.codility.com/programmers/lessons/5-prefix_sums/min_avg_two_slice/
+
+    print('INPUT ARRAY :{}'.format(A))
+    print('Prefixed sums is:{}'.format(new_array))
+
+    holder = []
+
+    for idx in range(2, len(new_array)):
+        tpl_idx = idx - 1
+        diff = idx - 2
+
+        for idx, item in enumerate(new_array[idx:]):
+            # To revise . we only look forward, not backwards, so we start from idx+1
+            # something not quite right we should hnot have anything lower than 2..
+            # we need to debug
+            holder.append( ((tpl_idx, tpl_idx + idx), item, (item - A[idx - 2]) / (idx + 2)))
+
+    return holder
 
