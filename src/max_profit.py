5b017d8bb894ff2be8cbbebfaf5c71e478825d41
mmistroni <mmistroni@gmail.com>
2022-06-07 18:49:18 +0100

    attempting golden max

--- src/max_profit.py
+++ src/max_profit.py
@@ -1,5 +1,6 @@
 from itertools import combinations
 def solution(A):
     tpl = [two - one for one, two in combinations(A, 2) if two > one]
-    return max(tpl)
+
+    return 0 if not tpl else max(tpl)
 
