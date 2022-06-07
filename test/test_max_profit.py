46d1b17374a4288340256066d15c4ec61218beb2
mmistroni <mmistroni@gmail.com>
2022-06-07 18:37:17 +0100

    attempting golden max

---
+++ test/test_max_profit.py
@@ -0,0 +1,10 @@
+import unittest
+from Codility.src.max_profit import solution
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        A = [23171, 21011, 21123, 21366, 21013, 21367]
+        self.assertEquals(356, solution(A))
+
+if __name__ == '__main__':
+    unittest.main()
