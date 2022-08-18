c6d8813a2b79c2ae10ea60a01b545f80ea8e0851
mmistroni <mmistroni@gmail.com>
2022-08-18 21:28:05 +0100

    adding count factors

--- src/min_perimeter_rectangle.py
+++ src/min_perimeter_rectangle.py
@@ -1,3 +1,4 @@
 def solution(N):
     #https://app.codility.com/programmers/lessons/10-prime_and_composite_numbers/min_perimeter_rectangle/
+    # Leverage the divisor method from count factors
     return 0
