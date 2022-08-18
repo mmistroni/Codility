1a5e10708e80b5d03e214f448187801e130daefc
mmistroni <mmistroni@gmail.com>
2022-08-18 21:26:46 +0100

    adding count factors with improvement

--- src/count_factors.py
+++ src/count_factors.py
@@ -2,7 +2,19 @@ import math
 from collections import deque
 from itertools import count
 
+def divisors(n):
+    i = 1
+    result = 0
+    while (i * i < n):
+        if (n % i == 0):
+            result += 2
+        i += 1
+    if (i * i == n):
+        result += 1
+    return result
+
 def solution(n):
+    return divisors(n)
     iterable = (i for i in range(1, n + 1) if n % i == 0)
     counter = count()
     deque(zip(iterable, counter), maxlen=0)  # (consume at C speed)
--- test/test_count_factors.py
+++ test/test_count_factors.py
@@ -1,11 +1,13 @@
 import unittest
-from src.count_factors import solution
+from src.count_factors import solution, divisors
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
 
         self.assertEquals(8, solution(24))
 
+    def test_something2(self):
+        print(divisors(24))
 
 
 if __name__ == '__main__':
