841b683ef067454c42a7ae940b23c57c81ce92de
mmistroni <mmistroni@gmail.com>
2022-09-13 20:48:17 +0100

    fixed triples

--- src/triangular_triplets.py
+++ src/triangular_triplets.py
@@ -1,21 +1,24 @@
 from itertools import combinations,dropwhile
 
-def check(tpl):
-    p, q, r = tpl
-
+def check(p, q, r):
     return (p + q) > r and \
     (q + r) > p and \
     (r + p) > q
 
+def _generate_result(s_a):
+    for i in range(0, len(s_a)):
+        if i+2 >= len(s_a):
+            break
+        p = s_a[i]
+        q = s_a[i + 1]
+        r = s_a[i + 2]
+        if check(p, q, r):
+            return 1
+    return 0
+
 def solution(A):
-    A.sort()
-
-    combis = combinations(A, 3)
-
-    res = dropwhile(lambda tpl: not check(tpl), combis)
-    try :
-        next(res)
-        return 1
-    except Exception as e:
+    if len(A) < 3:
         return 0
+    sorted_array = sorted(A, key=lambda x:x)
 
+    return _generate_result(sorted_array)
--- test/test_triangular_triplets.py
+++ test/test_triangular_triplets.py
@@ -6,6 +6,27 @@ class MyTestCase(unittest.TestCase):
         A = [ 10, 2, 5, 1, 8,20]
         self.assertEqual(1, solution(A))
 
+    def checker(self, p, q, r):
+        return (p + q) > r and \
+               (q + r) > p and \
+               (r + p) > q
+
+    def result(self, A):
+        s_a = sorted(A, key=lambda x: x)
+        for i in range(0, len(A) - 3):
+            p = s_a[i]
+            q = s_a[i + 1]
+            r = s_a[i + 2]
+            if self.checker(p, q, r):
+                return 1
+        return 0
+
+    def test_empty(self):
+        # expeted sol = Triplet (0, 2, 4) is triangular. 10 5 8
+        A = [5, 3, 3]
+        self.assertEqual(1, solution(A))
+
+
 
 if __name__ == '__main__':
     unittest.main()
