68ed400aff3510c97ef4ab870dfd4e496656743f
mmistroni <mmistroni@gmail.com>
2022-10-12 20:42:22 +0100

    fixing flags

--- src/flags.py
+++ src/flags.py
@@ -2,6 +2,36 @@
 # A peak is an array element which is larger than its neighbours.
 # More precisely, it is an index P such that 0 < P < N − 1 and A[P − 1] < A[P] > A[P + 1].
 from collections import namedtuple
+
+
+def check_flag_nums(peaks, flags):
+    found_flags = 0
+    last_flag = 0
+    for i in range(0, len(peaks)):
+        if i == 0:
+            found_flags += 1
+            last_flag = 0
+        else:
+            if peaks[i] - peaks[last_flag] >= flags:
+                found_flags += 1
+                if found_flags > flags:
+                    return flags
+                last_flag = i
+    return found_flags
+
+
+def evaluate(peaks):
+    '''
+    Flags can only be set on peaks.
+    What's more, if you take K flags,
+    then the distance between any two flags should be greater than or equal to K.
+    The distance between indices P and Q is the absolute value
+    '''
+    maxFlags = len(peaks)
+    flags = [check_flag_nums(peaks, i) for i in range(1, maxFlags)]
+    return max(flags)
+
+
 def solution(N):
     peaks = []
     start_idx = None
@@ -17,6 +47,6 @@ def solution(N):
             else:
                 if N[idx] > N[start_idx]:
                     continue
-    return peaks
+    return evaluate(peaks)
 
 
--- test/test_flags.py
+++ test/test_flags.py
@@ -4,25 +4,6 @@ from flags import solution
 
 class MyTestCase(unittest.TestCase):
 
-    def check(self, peaks, flags):
-        pass
-
-    def evaluate(self, peaks):
-        '''
-        Flags can only be set on peaks.
-        What's more, if you take K flags,
-        then the distance between any two flags should be greater than or equal to K.
-        The distance between indices P and Q is the absolute value
-        '''
-        fill_array = [0] * max(peaks) + 1
-        for p in peaks:
-            fill_array[p] = 1
-        from pprint import pprint
-        pprint(fill_array)
-
-        # Possible solution we need to loop , add flags
-        # and check , at every step, if we have a flag at idx+4
-
 
 
     def test_something(self):
@@ -42,9 +23,7 @@ class MyTestCase(unittest.TestCase):
         A[11] = 2
 
         peaks = solution(A)
-        print(peaks)
-        self.assertEqual(3, solution(A))
-
+        self.assertEqual(3, peaks)
 
 if __name__ == '__main__':
     unittest.main()
