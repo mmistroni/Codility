5155f9272b78828d1d028279943b71020dc5e336
mmistroni <mmistroni@gmail.com>
2022-11-15 21:23:01 +0000

    adding drawing book

--- src/valleys.py
+++ src/valleys.py
@@ -1,4 +1,38 @@
 # https://www.hackerrank.com/challenges/counting-valleys/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
 
-def solution():
-    return 0
+'''
+A mountain is a sequence of consecutive steps above sea level,
+starting with a step up from sea level and ending with a step down to sea level.
+A valley is a sequence of consecutive steps below sea level,
+starting with a step down from sea level and ending with a step up to sea level.
+
+'''
+SEA = 0
+
+def find_valleys(lst):
+    num_valleys = 0
+    in_valley = False
+    while lst:
+        item = lst.pop()
+        if item < 0:
+            in_valley = True
+        else:
+            if in_valley:
+                num_valleys +=1
+                in_valley = False
+    return num_valleys
+
+
+def solution(steps, path):
+    state = SEA
+    mapped = []
+    tolist = list(path)
+    while tolist:
+        step = tolist.pop(0)
+        if step == "U":
+            state +=1
+        else:
+            state -=1
+        mapped.append(state)
+    print(mapped)
+    return find_valleys(mapped)
---
+++ test/test_valleys.py
@@ -0,0 +1,16 @@
+import unittest
+from valleys import solution
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        steps = 8
+        path = "UDDDUDUU"
+        self.assertEqual(1, solution(steps, path))
+
+    def test_something(self):
+        steps = 12
+        path = "DDUUDDUDUUUD"
+        self.assertEqual(2, solution(steps, path))
+
+if __name__ == '__main__':
+    unittest.main()
