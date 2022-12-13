d67e0379c2fd96cff1bb5a6707bd711af67bfed0
mmistroni <mmistroni@gmail.com>
2022-12-13 21:20:16 +0000

    attemptign carol of code

--- src/carol_of_code.py
+++ src/carol_of_code.py
@@ -1,5 +1,9 @@
 #https://app.codility.com/c/run/certMRYJ6F-KD2WB54H4CD59SJQ/
 
+# the connection will be done between
+#item[0][]2  and item[1][4]
+
+
 
 def  solution(input_array):
     return 0
--- test/test_carol_of_code.py
+++ test/test_carol_of_code.py
@@ -2,6 +2,35 @@ import unittest
 from carol_of_code import solution
 
 class MyTestCase(unittest.TestCase):
+
+    def rotate(self, item, clockwise=True):
+        if not clockwise:
+            return item[1:] + [item[0]]
+        else:
+            return [item[-1]] + item[0:3]
+
+
+    def test_rotate(self):
+        inarr = list("WRGB")
+        print(f'Clockwise: in:{inarr}, out:{self.rotate(inarr)}')
+
+
+
+    def find_closest(self, left, right):
+        right_side = left[2]
+        left_side = right[4]
+
+        if right_side == left_side:
+            return 0
+        else:
+            right_in_right = right.find(right_side)
+            left_in_left = left.find()
+
+
+
+
+
+
     def test_something(self):
         A =  ["RGBW", "GBRW"],
         self.assertEqual(1, solution(A))
