ce03287c5a8b81b92f5ff5b46205be06965c9ad1
mmistroni <mmistroni@gmail.com>
2022-12-29 22:32:04 +0000

    fixing julian calendar

--- src/day_of_programmer.py
+++ src/day_of_programmer.py
@@ -1,22 +1,52 @@
 #https://www.hackerrank.com/challenges/day-of-the-programmer/problem?isFullScreen=true
 
-days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day_of_programmer = 256
+
+calendar_dict = {'Julian' :  ([29, 28, 31, 29, 31, 29, 31, 29],
+                              [29, 23, 27, 31, 29, 31, 29, 31, 29]),
+                 'Interim':  ([31, 14, 31, 30, 31, 30, 31, 31],
+                              [31, 14, 31, 30, 31, 30, 31, 31]),
+                 'Gregorian' : ([31, 28, 31, 30, 31, 30, 31, 31],
+                                [31, 29, 31, 30, 31, 30, 31, 31])}
 
 
-def is_leap(year):
-    if year % 400 == 0:
-        return True
-    if year % 4 == 0 and year % 100 !=0:
-        return True
+
+def is_leap(year, key):
+    if 'Gregorian' in key:
+        if year % 400 == 0:
+            return True
+        if year % 4 == 0 and year % 100 !=0:
+            return True
+        return False
+    elif 'Julian' in key:
+        return year % 4 == 0
     return False
 
-def is_gregorian(year):
-    return False
+
+def get_calendar(year):
+    if year <=1917:
+        return 'Julian'
+    elif year == 1918:
+        return 'Interim'
+    return 'Gregorian'
 
 
 def solution(year):
-    if is_leap(year):
-        days[1] = 29
-    
+
+    calname = get_calendar(year)
+
+    tpls = calendar_dict.get(calname)
+
+    if is_leap(year, calname):
+        item  = tpls[1]
+    else:
+        item = tpls[0]
+
+    all_dt = 256 - sum(item)
+
+    return f'{all_dt}.9.{year}'
+
+
+
 
     prog_day = 256
---
+++ test/test_day_of_programmer.py
@@ -0,0 +1,25 @@
+from day_of_programmer import solution, is_leap, get_calendar
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def test_is_leap(self):
+
+        tpls = [
+                 (1984, True),
+                 (2017, False),
+                 (2016, True),
+                 (1800, True)
+
+                 ]
+        for expected, res in tpls:
+            calname = get_calendar(expected)
+            self.assertEqual(res, is_leap(expected, calname), expected)
+
+
+    def test_julian_year(self):
+        res = solution(1917)
+        print(res)
+
+if __name__ == '__main__':
+    unittest.main()
+
