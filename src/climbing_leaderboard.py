660d311cd4628786c664f2e206ebac47ea97956a
mmistroni <mmistroni@gmail.com>
2023-01-05 21:01:08 +0000

    new attempt

--- src/climbing_leaderboard.py
+++ src/climbing_leaderboard.py
@@ -4,13 +4,13 @@ from heapq import heappush, heapify, nlargest
 
 
 def solution(ranked, player):
-    tst =  [i * -1 for i in set(ranked)]
     holder = []
     for item in player:
-        new_item = item *-1
-        tst.append(new_item)
+        tst = [i * -1 for i in set(ranked)]
         heapify(tst)
+        new_item = item * -1
+        heappush(tst, new_item)
         idx = tst.index(new_item) + 1
         holder.append(idx)
-        tst.remove(new_item)
+
     return holder
