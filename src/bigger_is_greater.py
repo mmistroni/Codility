44c1e5a2bd56adc94a2437727e8b37ddc10645fe
mmistroni <mmistroni@gmail.com>
2023-01-10 20:54:31 +0000

    adding bigger isbetter

---
+++ src/bigger_is_greater.py
@@ -0,0 +1,4 @@
+#https://www.hackerrank.com/challenges/bigger-is-greater/problem?isFullScreen=true
+
+def solution(w):
+    return -1
---
+++ test/test_bigger_is_greater.py
@@ -0,0 +1,33 @@
+import unittest
+
+from bigger_is_greater import solution
+
+class MyTestCase(unittest.TestCase):
+
+
+    def test_something(self):
+        res = solution('ab')
+        self.assertEqual('ba', res)
+
+
+    def test_something2(self):
+        res = solution('bb')
+        self.assertEqual('no answer', res)
+
+    def test_something3(self):
+        res = solution('hefg')
+        self.assertEqual('hegf', res)
+
+    def test_something4(self):
+        res = solution('dhck')
+        self.assertEqual('dhkc', res)
+
+    def test_something5(self):
+        res = solution('dkhc')
+        self.assertEqual('hcdk', res)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
