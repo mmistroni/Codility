d8898e03c46c19d7fa3625a1772babac2c26e84d
mmistroni <mmistroni@gmail.com>
2023-01-10 20:47:40 +0000

    fixed leaderboard£

--- src/climbing_leaderboard.py
+++ src/climbing_leaderboard.py
@@ -1,19 +1,18 @@
 #https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem?h_r=next-challenge&h_v=zen
 
-from heapq import heappush, heapify, nlargest
+from heapq import heappush, heapify, nlargest, nsmallest
 
 
 def solution(ranked, player):
     holder = []
     for item in player:
         tst = [i * -1 for i in set(ranked)]
+        new_item  = item * -1
+        tst.append(new_item)
+        tst  = list(tst)
         heapify(tst)
-        new_item = item * -1
-        heappush(tst, new_item)
-
-        elems = nlargest(tst, len(tst))
-
-        idx = elems.index(new_item) + 1
+        elems = nsmallest(len(tst), tst)
+        idx = elems.index(new_item) + 1 % 6
         holder.append(idx)
 
     return holder
