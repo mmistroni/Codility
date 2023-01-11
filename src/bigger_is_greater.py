f37b9451c879ef992b2a4bcc55389d42e463e53d
mmistroni <mmistroni@gmail.com>
2023-01-11 07:33:23 +0000

    committig bigger is better first run

--- src/bigger_is_greater.py
+++ src/bigger_is_greater.py
@@ -1,4 +1,16 @@
 #https://www.hackerrank.com/challenges/bigger-is-greater/problem?isFullScreen=true
 
 def solution(w):
-    return -1
+
+    mapped = [ord(c) for c in w]
+
+    for idx in range(len(mapped)-1, -1, -1):
+        if idx > 0:
+            cur = mapped[idx]
+            nxt = mapped[idx-1]
+            if cur > nxt:
+                mapped[idx] = nxt
+                mapped[idx-1] = cur
+                return ''.join([chr(n) for n in mapped])
+
+    return "no answer"
