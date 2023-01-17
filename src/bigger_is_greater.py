84ebc6484cd59b3d56b5fdc8b310f66d7eda9a5b
mmistroni <mmistroni@gmail.com>
2023-01-17 20:50:10 +0000

    first try

--- src/bigger_is_greater.py
+++ src/bigger_is_greater.py
@@ -1,4 +1,16 @@
 #https://www.hackerrank.com/challenges/bigger-is-greater/problem?isFullScreen=true
 
-def solution(w):
-    return -1
+def solution(x):
+
+    w = list(x)
+    for i in range(0, len(w)):
+        if i < len(w)-1:
+            prev = ord(w[i])
+            nxt = ord(w[i+1])
+            if prev > nxt:
+                w[i], w[i+1] = w[i+1], w[i]
+                return ''.join(w)
+
+
+
+    return "not possible"
