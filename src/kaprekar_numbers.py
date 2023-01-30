a0bbb83ab4478173b6e08bc82bc67b1d8f999b30
mmistroni <mmistroni@gmail.com>
2023-01-30 20:33:01 +0000

    adding kaprekar

--- src/kaprekar_numbers.py
+++ src/kaprekar_numbers.py
@@ -1,4 +1,18 @@
 #https://www.hackerrank.com/challenges/kaprekar-numbers/problem?isFullScreen=false&h_r=next-challenge&h_v=zen
 
-def solution(start):
-    return -1
+import math
+
+
+def is_kaprekar(num):
+    squared = str(num ** 2)
+
+    if len(squared) >= 2:
+        mid_point = len(squared) // 2
+        summed = int(squared[0:mid_point]) + int(squared[mid_point:])
+        return summed == num
+    else:
+        return int(squared) == num
+    return False
+def solution(p, q):
+    return [i for i in range(p, q) if is_kaprekar(i)]
+
--- test/test_kaprekar_numbers.py
+++ test/test_kaprekar_numbers.py
@@ -3,7 +3,13 @@ import unittest
 from kaprekar_numbers import solution
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        p, q = 1,100
+
+        expected = [1, 9, 45, 55, 99]
+
+        res = solution(p, q)
+
+        self.assertEqual(expected, res)
 
 
 if __name__ == '__main__':
