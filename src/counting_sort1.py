bb943488b4f89abf0834a23883613a2a84708e36
mmistroni <mmistroni@gmail.com>
2023-02-14 21:12:11 +0000

    addign counting sort

--- src/counting_sort1.py
+++ src/counting_sort1.py
@@ -1,4 +1,8 @@
 #https://www.hackerrank.com/challenges/countingsort1/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
 
 def solution(input_array):
-    return 0
+    arr = [0] * len(input_array)
+    for item in input_array:
+        arr[item] += 1
+
+    return arr[0:100]
