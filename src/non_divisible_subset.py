3771790a688173a9caf52b5f73da238867bd5a42
mmistroni <mmistroni@gmail.com>
2023-02-14 21:20:40 +0000

    addign nondiv subset

---
+++ src/non_divisible_subset.py
@@ -0,0 +1,4 @@
+##https://www.hackerrank.com/challenges/non-divisible-subset/problem?isFullScreen=true
+
+def solution(arr, k):
+    return 0
---
+++ test/test_nondivisible_subset.py
@@ -0,0 +1,23 @@
+import unittest
+from non_divisible_subset import solution
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+
+        S = [1, 7, 2, 4]
+        k = 3
+
+        res = solution(S, k)
+        self.assertEqual(3, res)
+
+
+    def test_something2(self):
+        S = [19, 10, 12,10, 24, 25,22]
+        k = 4
+
+        res = solution(S, k)
+        self.assertEqual(3, res)
+
+
+if __name__ == '__main__':
+    unittest.main()
