85f1dffc38fb841ee2af95c6a6d7b9dc20768277
mmistroni <mmistroni@gmail.com>
2023-02-15 21:28:33 +0000

    non div subset comments

--- src/non_divisible_subset.py
+++ src/non_divisible_subset.py
@@ -1,4 +1,16 @@
 ##https://www.hackerrank.com/challenges/non-divisible-subset/problem?isFullScreen=true
-
+from itertools import combinations
 def solution(arr, k):
-    return 0
+    good_ones = (i for i in arr if i%k !=0)
+    '''
+     #https://medium.com/@mrunankmistry52/non-divisible-subset-problem-comprehensive-explanation-c878a752f057
+     Here's my take on this problem. I deconstructed all the numbers into num%k because we don't really care about the whole number
+      - just it's remainder. 
+      for each i in <1, k-1> (that is results of modulo) we have exclusive unique unordered pairs (i, k-i) 
+      (eg. k=5 : (1, 4), (2, 3) that would sum to k if added, 
+      therefore we add to the count only the max number of occurences from the pair counts. 
+      We can also always add only one 0 and if k%2==0 one k/2 number.
+    '''
+
+
+
