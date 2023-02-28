89b430d81d68fa0b7e70bca3d51c6dd5e01dacc1
mmistroni <mmistroni@gmail.com>
2023-02-28 21:25:48 +0000

    addign funny strings (wrong)

--- src/funny_strings.py
+++ src/funny_strings.py
@@ -1,3 +1,5 @@
+#https://www.hackerrank.com/challenges/funny-string/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+
 def solution(s):
     r = s[::-1]
 
@@ -5,10 +7,10 @@ def solution(s):
 
     second = [ord(c) for c in r]
 
-    diff_one = [first[i+1] - first[i] for i in range(0, len(l)-1)]
+    diff_one = [abs(first[i] - first[i+1]) for i in range(0, len(first)-1)]
 
-    diff_two = [second[i + 1] - second[i] for i in range(0, len(l) - 1)]
+    diff_two = [abs(second[i] - second[i+1]) for i in range(0, len(second) - 1)]
 
-    return "Funny" if diff_one == diff_two else "Not funny"
+    return "Funny" if diff_one == diff_two else "Not Funny"
 
 
--- test/test_funny_strings.py
+++ test/test_funny_strings.py
@@ -16,6 +16,49 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual("Not Funny", solution(s))
 
 
+    def test_something4(self):
+        s = 'ovyvzvptyvpvpxyztlrztsrztztqvrxtxuxq'
+        self.assertEqual("Not Funny", solution(s))
+
+    def test_something5(self):
+        s = 'holtm'
+        self.assertEqual("Funny", solution(s))
+
+    def test_something6(self):
+        s = 'uvzxrumuztyqyvpnji'
+        self.assertEqual("Funny", solution(s))
+
+    def test_something7(self):
+        s = '10'
+        self.assertEqual("Funny", solution(s))
+
+
+''''
+10
+ovyvzvptyvpvpxyztlrztsrztztqvrxtxuxq
+holtm
+uvzxrumuztyqyvpnji
+tmruzxzuwoskqysxztuvosuyrswrnmtxvzsrqwytzrxpltrwusxupw
+wxstwxuzuyuvyzrsxysxyuvyqxuxyskqwsyqumqrvopvowqumnvrxpwqpwsrnvrztxrxpvuxunvyzvupvupowvyzvzuzwvsrwv
+yrzxrxskrtlpwpmtpxvowrxrpxq
+pryumtuntmovpwvowslj
+nosklrxrtyuxtmnurzsryuxtywqwqpxts
+fmpszyvqwxrtvpuwqszvyvotmsxsxuvzyvpwzrpmuxqwtswvytytzsnuxuyrpvtysqoutzurqxury
+jkmsxzwrxzy
+
+Funny
+Not Funny
+Funny
+Funny
+Funny
+Not Funny
+Funny
+Not Funny
+Not Funny
+Not Funny
+
+'''
+
 
 if __name__ == '__main__':
     unittest.main()
