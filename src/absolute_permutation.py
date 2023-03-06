5aa0a545f20826392be2d2d91fbe12f0aaa7e1df
mmistroni <mmistroni@gmail.com>
2023-03-06 21:12:28 +0000

    adding absolute permutation

--- src/absolute_permutation.py
+++ src/absolute_permutation.py
@@ -1,4 +1,19 @@
 #https://www.hackerrank.com/challenges/absolute-permutation/problem?isFullScreen=true
 
 def solution(n , k):
-    return 0
+    # Need a lexicographically permutation. see test/test_bigger_is_greater.py. oor mayb not
+    # we loop from 0 to len(n) and check if there is an elem
+    lst = list(range(1, n+1))
+    holder = []
+    for i in range(1, n+1):
+        #  is considered to be an absolute permutation if [pos[i] - i| =k  holds true for every  pos[i]. = k + i
+        candidate = k + i if i <= k else i -k
+        if candidate in lst:
+            holder.append(candidate)
+        else:
+            return [-1]
+    return holder
+
+
+
+
--- test/test_absolute_permutation.py
+++ test/test_absolute_permutation.py
@@ -4,7 +4,75 @@ from absolute_permutation import solution
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        n = 4
+        k = 2
+        x = list(range(1, n+1))
+
+        expected = [3, 4, 1, 2]
+        self.assertEqual(expected, solution(n, k))
+
+    def test_something2(self):
+        n = 2
+        k = 1
+        expected = [2, 1]
+        self.assertEqual(expected, solution(n, k))
+
+    def test_something3(self):
+        n = 3
+        k = 0
+        expected = [1, 2, 3]
+        self.assertEqual(expected, solution(n, k))
+
+    def test_something4(self):
+        n = 3
+        k = 2
+        expected = [-1]
+        self.assertEqual(expected, solution(n, k))
+
+    def test_something5(self):
+        n = 10
+        k = 0
+        expected = list(range(1,11))
+        self.assertEqual(expected, solution(n, k))
+
+    def test_something6(self):
+        n = 10
+        k = 1
+        expected = [2, 1, 4, 3, 6, 5, 8, 7, 10, 9]
+        self.assertEqual(expected, solution(n, k))
+
+        ''''
+        inputs
+        10 0
+10 1
+7 0
+10 9
+9 0
+10 3
+8 2
+8 0
+7 0
+10 1
+        
+        outputs
+        1 2 3 4 5 6 7 8 9 10
+2 1 4 3 6 5 8 7 10 9
+1 2 3 4 5 6 7
+-1
+1 2 3 4 5 6 7 8 9
+-1
+3 4 1 2 7 8 5 6
+1 2 3 4 5 6 7 8
+1 2 3 4 5 6 7
+2 1 4 3 6 5 8 7 10 9
+        
+        
+        '''
+
+
+
+
+
 
 
 if __name__ == '__main__':
