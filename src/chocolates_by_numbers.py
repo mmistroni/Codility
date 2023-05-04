1fc39d84f66741709bc8a989acb0383f4313f71e
mmistroni <mmistroni@gmail.com>
2023-05-04 21:14:20 +0100

    addinc omments

--- src/chocolates_by_numbers.py
+++ src/chocolates_by_numbers.py
@@ -3,14 +3,15 @@ import logging
 
 def solution(N, M):
     # Not good enough, this has to do with euclidean algorithm
-    ## it's supposed to run in 0.1 seconds 
-    choc_array = [1] * N
+    ## it's supposed to run in 0.1 seconds
+    choc_array = [1] * N # we dont need to hold array. we just looop thru
+                 # numbers and see if we have seen the current
     next = 0
     holder = []
     while(choc_array[next] != 0):
         #logging.info(f'Eating {next}')
         choc_array[next] = 0
         holder.append(next)
-        next = (next + M) % N
+        next = (next + M) % N  # this is being done in euclidan algo
 
     return len(holder)
