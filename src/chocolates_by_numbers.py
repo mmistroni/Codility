7f713c33549fcf303bab6261c8eb93a7e089f65e
mmistroni <mmistroni@gmail.com>
2023-05-09 21:05:50 +0100

    anoter try at chocolate. we shud invest in binary gcd

--- src/chocolates_by_numbers.py
+++ src/chocolates_by_numbers.py
@@ -3,6 +3,7 @@ import logging
 
 def solution(N, M):
     # Not good enough, this has to do with euclidean algorithm
+    # hint. use binary bcd
     ## it's supposed to run in 0.1 seconds
     choc_array = [1] * N # we dont need to hold array. we just looop thru
                  # numbers and see if we have seen the current
--- test/test_chocolates_by_numbers.py
+++ test/test_chocolates_by_numbers.py
@@ -2,7 +2,31 @@ import unittest
 
 from chocolates_by_numbers import solution
 
+def gcd(a, b, res):
+  if a == b:
+    return res * a
+  elif (a % 2 == 0) and (b % 2 == 0):
+    return gcd(a // 2, b // 2, 2 * res)
+  elif (a % 2 == 0):
+    return gcd(a // 2, b, res)
+  elif (b % 2 == 0):
+    return gcd(a, b // 2, res)
+  elif a > b:
+    return gcd(a - b, b, res)
+  else:
+    return gcd(a, b - a, res)
+
+
+
 class MyTestCase(unittest.TestCase):
+
+
+    def test_gcd(self):
+        N, M = 10, 4
+        res= gcd(N ,M, N % M)
+        print(res)
+
+
     def test_something(self):
         N, M = 10, 4
         expected = len([0, 4, 8, 2, 6])
