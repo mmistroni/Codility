2ba3f489434a23d0d29626ef474b11710cd9ab23
mmistroni <mmistroni@gmail.com>
2023-05-09 21:09:44 +0100

    anoter try at chocolate. we shud invest in binary gcd

--- src/chocolates_by_numbers.py
+++ src/chocolates_by_numbers.py
@@ -1,6 +1,7 @@
 #https://app.codility.com/programmers/lessons/12-euclidean_algorithm/chocolates_by_numbers/
 import logging
 # https://www.researchgate.net/post/What-is-the-recurrence-relation-for-the-Euclidean-GCD-Algorithm
+#https://www.hackerearth.com/practice/notes/euclid-algorithm-made-easy-1/#:~:text=There%20is%20a%20recurrence%20relation%20of%20the%20function,the%20base%20case%20is%20GCD%20%28a%2C0%29%20%3D%20a%3B
 def solution(N, M):
     # Not good enough, this has to do with euclidean algorithm
     # hint. use binary bcd
