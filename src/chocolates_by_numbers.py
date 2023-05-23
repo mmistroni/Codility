6758aeee91704d0d3347faf42bf423e132a852b6
mmistroni <mmistroni@gmail.com>
2023-05-23 21:20:47 +0100

    added new problem

--- src/chocolates_by_numbers.py
+++ src/chocolates_by_numbers.py
@@ -4,9 +4,9 @@ import logging
 #https://www.hackerearth.com/practice/notes/euclid-algorithm-made-easy-1/#:~:text=There%20is%20a%20recurrence%20relation%20of%20the%20function,the%20base%20case%20is%20GCD%20%28a%2C0%29%20%3D%20a%3B
 
 #https://www.researchgate.net/post/What-is-the-recurrence-relation-for-the-Euclidean-GCD-Algorithm
-
-
 #https://www.cuemath.com/numbers/euclids-division-algorithm/
+
+# Another hint https://www.britannica.com/science/number-theory/Euclid
 
 def gcd(a, b, res, holder):
   if a in holder: # b and res are unique, but a is not
--- src/common_prime_divisors.py
+++ src/common_prime_divisors.py
@@ -1,4 +1,6 @@
 #https://app.codility.com/programmers/lessons/12-euclidean_algorithm/common_prime_divisors/
 
+
+# Hint : https://www.britannica.com/science/number-theory/Euclid
 def solution(N, M):
     return 0
