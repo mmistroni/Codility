2baa80b34e9184ba7570dea71a8029d02f093234
mmistroni <mmistroni@gmail.com>
2023-09-06 20:49:20 +0100

    trying to  refine

--- src/fib_frog.py
+++ src/fib_frog.py
@@ -9,54 +9,36 @@ def fibonacci_seq(n, limit=None):
     fib[1] = 1
     for i in range(2, n+1):
         fib[i] = fib[i-1] + fib[i-2]
+        if limit and fib[i] == limit:
+            return fib
 
     return [f for f in fib if f <= limit] if limit else fib
 
 def new_algo(A):
 
-    ''' TODO  Need to handle this special case which we didnt consider
-
-        The goal is to count the minimum number of jumps in which the frog can get to the other side of the river
-         (from position −1 to position N).
-        The frog can jump between positions −1 and N (the banks of the river) and every position containing a leaf.
-
-
-    '''
-
-
-    # The frog can jump over any distance F(K), where F(K) is the K-th Fibonacci number.
-    # Not quite right.
-    # 1. we need to exclude the zeros as it's not a jump
-    # 2. We need to look at combinations from 2 to max ones
-    # 3. We start with  two jumps.if not we do combi of 3
-    # 1. Find good fibonacci seq which covers potential jumps in the array
-
     one_jump = len(A) + 1
-
-
-    fib_seq = [f for f in  fibonacci_seq(len(A) + 1, len(A)) if f > 0]
+    fib_seq = [f for f in  fibonacci_seq(len(A) + 2, len(A)+2) if f > 0]
 
     if one_jump in fib_seq:
+        # direct jump to the end
         return 1
 
+    # Looking for leafs
 
     # Find how many ones
-    ones = [idx for idx in range(0, len(A)) if A[idx] == 1] + [len(A) + 1]
+    ones = [idx for idx in range(0, len(A)) if A[idx] == 1]
 
     if not ones:
         return -1
 
+    # adding start and end
+    ones = [-1] + ones +  [len(A) + 1]
 
-    diffs = [(idx+1) for idx in ones]
+    diffs = [(idx+1) for idx in ones] # difference between first element and all subsequent 1s
 
     combilen = len(ones)
 
-    # 2, all possible combinations of fibonacci which sums
-    # to len(A) + 1
-
-    good_tpls = []
-
-
+    # Looping for all possible combination of fibonacci numbers which sums up to len[A] + 1
     for clen in range(2, combilen+1):
         for p in product(fib_seq, repeat=clen):
             if sum(p) == len(A) + 1:
--- test/test_fib_frog.py
+++ test/test_fib_frog.py
@@ -7,266 +7,41 @@ from itertools import product, combinations_with_replacement
 
 
 import timeit
-
-def fibonacci_seq(n, limit=None):
-    fib = [0] * (n+2)
-    fib[1] = 1
-    for i in range(2, n+1):
-        fib[i] = fib[i-1] + fib[i-2]
-
-        # Re-add the limit..
-
-    return [f for f in fib if f <= limit] if limit else fib
-
-def fibonacci2(n):
-    if n == 0:
-        return 0
-    fib = [0] * (n+1)
-    fib[1] = 1
-    for i in range(2, n+1):
-        fib[i] = fib[i-1] + fib[i-2]
-    return fib[n]
-
-def find_next(start, nextes, fib_dict):
-    holder = []
-    for i in nextes:
-        diff = i - start
-        if diff in fib_dict.keys():
-            holder.append(i)
-
-    return holder
-
-
-def find_all_trees(A):
-    def find_all_next(start_idx, remainder, fib_dict):
-        return [i for i in remainder if (i - start_idx) in fib_dict.keys()]
-
-    seq = fibonacci_seq(max(len(A), 10))[1:]
-    fib_dict = dict((seq[i], i) for i in range(0, len(seq)))
-    ones = [idx for idx in range(0, len(A)) if A[idx] == 1]
-
-    adj_list_dict = {}
-    ones = [-1] + ones + [len(A)]
-    for idx, item in enumerate(ones):
-        nxts  = find_all_next(item, ones[idx+1:], fib_dict)
-        adj_list_dict[item] = nxts
-    adj_list_dict[ones[-1]] = ones[-1]
-    return adj_list_dict
-
-
-
-def find_jumps(A):
-    def dfs(node, graph, visited, component, end):
-        component.append(node)  # Store answer
-        visited[node] = True  # Mark visited
-        if end in component:  # Not quite there. if a component has no children we need to remove it
-            return
-        for child in graph[node]:
-            if not visited.get(child) and graph.get(child):  # Check whether the node is visited or not
-                return dfs(child, graph, visited, component, end)
-
-
-    # Not quite there. we need to check the indexes that got he
-    graph = find_all_trees(A)
-
-    component = []
-    visited = {}
-
-    dfs(-1, graph, visited, component, len(A))
-    jumps = len(component) - 1
-
-    return jumps
-
-
 from collections import defaultdict
 
 
-class Graph:
-
-    def __init__(self, nodes: int, fib_dict):
-        # Store the adjacency list as a dictionary
-        # { 0 : [ 1, 2 ], 1 : [ 3, 4 ] }
-
-        # The default dictionary would create an empty list as a default (value)
-        # for the nonexistent keys.
-        self.adjlist = defaultdict(list)
-        self.nodes = nodes
-        self.fib_dict = fib_dict
-
-    # Assuming that the edge is bidirectional
-    def AddEdge(self, src: int, dst: int):
-        diff = dst -src
-        if diff in self.fib_dict:
-            self.adjlist[src].append(dst)
-        #self.adjlist[dst].append(src)
-
-    def Display_AdjList(self):
-        for item in self.adjlist.items():
-            print(item)
-
-    def find_shortest(self):
-
-        def dijkstra(graph, source):
-            distance = {node: float('infinity') for node in self.nodes}
-
-
-            distance[source] = 0
-            queue = [(0, source)]
-            while queue:
-                current_distance, current_node = heapq.heappop(queue)
-
-                if current_node == self.nodes[-1]:
-                    print('Exiting..')
-                    break
-                if current_distance > distance[current_node]:
-                    continue
-
-                for neighbor  in graph[current_node]:
-                    distance_through_current_node = current_distance + 1
-
-                    if distance_through_current_node < distance[neighbor]:
-                        distance[neighbor] = distance_through_current_node
-                        heapq.heappush(queue, (distance[neighbor], neighbor))
-
-            return distance[current_node]
-
-        return dijkstra(self.adjlist, -1)
-
-    def BFS_SP(self):
-        graph = self.adjlist
-        start = self.nodes[0]
-        goal = self.nodes[-1]
-        explored = []
-
-        # Queue for traversing the
-        # graph in the BFS
-        queue = [[start]]
-
-        # If the desired node is
-        # reached
-        if start == goal:
-            print("Same Node")
-            return
-
-        # Loop to traverse the graph
-        # with the help of the queue
-        while queue:
-            path = queue.pop(0)
-            node = path[-1]
-
-            # Condition to check if the
-            # current node is not visited
-            if node not in explored:
-                neighbours = graph[node]
-
-                # Loop to iterate over the
-                # neighbours of the node
-                for neighbour in neighbours:
-                    new_path = list(path)
-                    new_path.append(neighbour)
-                    queue.append(new_path)
-
-                    # Condition to check if the
-                    # neighbour node is the goal
-                    if neighbour == goal:
-                        print("Shortest path = ", *new_path)
-                        return len(new_path)
-                explored.append(node)
-
-        # Condition when the nodes
-        # are not connected
-        print("So sorry, but a connecting" \
-              "path doesn't exist :(")
-        return -1
 
 class MyTestCase(unittest.TestCase):
-
-
-    def new_algo(self, A):
-
-        ''' TODO  Need to handle this special case which we didnt consider
-
-            The goal is to count the minimum number of jumps in which the frog can get to the other side of the river
-             (from position −1 to position N).
-            The frog can jump between positions −1 and N (the banks of the river) and every position containing a leaf.
-
-
-        '''
-
-
-        # The frog can jump over any distance F(K), where F(K) is the K-th Fibonacci number.
-        # Not quite right.
-        # 1. we need to exclude the zeros as it's not a jump
-        # 2. We need to look at combinations from 2 to max ones
-        # 3. We start with  two jumps.if not we do combi of 3
-        # 1. Find good fibonacci seq which covers potential jumps in the array
-
-        one_jump = len(A) + 1
-
-
-        fib_seq = [f for f in  fibonacci_seq(len(A) + 1, len(A)) if f > 0]
-
-        if one_jump in fib_seq:
-            return 1
-
-
-        # Find how many ones
-        ones = [idx for idx in range(0, len(A)) if A[idx] == 1] + [len(A) + 1]
-
-        if not ones:
-            return -1
-
-
-        diffs = [(idx+1) for idx in ones]
-
-        combilen = len(ones)
-
-        # 2, all possible combinations of fibonacci which sums
-        # to len(A) + 1
-
-        good_tpls = []
-
-
-        for clen in range(1, combilen+1):
-            for p in product(fib_seq, repeat=clen):
-                if sum(p) == len(A) + 1:
-                    if p[0] in diffs:
-                        #good_tpls.append(p)
-                        return  len(p)
-        return -1
-
 
 
     def test_newtest2(self):
         print('---- NEXT --')
         A = [1, 1, 0, 0, 0]
         res = solution(A)
-        res2 = self.new_algo(A)
-        print(f'-- distance to end is:{res} vs {res2}')
         self.assertEquals(2, res)
 
     def test_single(self):
         print('---- NEXT --')
         A = [1]
-        res2 = self.new_algo(A)
+        res = solution(A)
         self.assertEquals(1, res)
 
 
     def test_fibfrog(self):
         #The frog can jump over any distance F(K), where F(K) is the K-th Fibonacci number.
         A = [1, 1, 1]#[0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0]
-        res = self.new_algo(A)
+        res = solution(A)
         self.assertEquals(2, res)
 
     def test_fibfrog_1(self):
         A = [0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0]
-        res = self.new_algo(A)
+        res = solution(A)
         self.assertEquals(3, res)
 
 
     def test_fibfrog2(self):
         A = []
-        jumps = self.new_algo(A)
+        jumps = solution(A)
         #res = self.probe(A)
         #res = solution(A)
 
@@ -275,59 +50,30 @@ class MyTestCase(unittest.TestCase):
     def test_fibfrog3(self):
         A = [1]
         jumps = solution(A)
-        jumps2 = self.new_algo(A)
-        print(f'jUMPS {jumps} vs {jumps2}')
-        self.assertEquals(1, jumps2)
+        self.assertEquals(1, jumps)
 
     def test_fibfrog4(self):
         A = [1, 1, 1]
-        res = self.new_algo(A)
+        res = solution(A)
         self.assertEquals(2, res)
 
     def test_zeros(self):
         A = [0, 0, 0]
-        jumps = self.new_algo(A)
+        jumps = solution(A)
         self.assertEquals(-1, jumps)
 
     def test_anotherfail(self):
         A =  [0, 0, 0, 1, 0]
-        jumps =self.new_algo(A) #solution(A)
+        jumps = solution(A) #solution(A)
         self.assertEquals(-1, jumps)
 
     def test_anotheranotherfail(self):
         A = [1, 1, 0, 0, 0]
 
-        #res = fibonacci_seq(len(A) + 1, len(A))
-
-        ones = [idx for idx in range(0, len(A)) if A[idx] == 1]
-        ones = [-1] + ones + [len(A)]
-
-        # Strategy #1. find all possible differences between the ones
-
-        #jumps = 2
-        res = self.new_algo(A)
+        res = solution(A)
         self.assertEquals(2, res)
         #https://www.geeksforgeeks.org/python-program-for-dijkstras-shortest-path-algorithm-greedy-algo-7/
 
-
-
-    def test_fib_sequence(self):
-        # Promising. But we'll need next step now. So we only have 27 numbers
-        # which are less than 100k
-        # Then we need to find max distance between start and any items to the end, that will narrow down
-        # the numbers to consider as that will be the first jump
-        # then comes the difficult part we have to think about as we should find every possible
-        # diffs betweeh 1s and see if they are fib numbers.
-        start = time.time()
-        res = fibonacci_seq(100000, 100000)
-        end = time.time()
-        print(f'{end - start} = {len(res)}')
-
-
-    def test_combilen(self):
-        fib_seq = [1,1,2,3,5,8]
-        for p in product(fib_seq, repeat=3):
-            print(p)
 
 if __name__ == '__main__':
     unittest.main()
