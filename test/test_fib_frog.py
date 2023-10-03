dd20e06b6c59c2316e762c4810f64c2a1128582a
mmistroni <mmistroni@gmail.com>
2023-10-03 21:04:06 +0100

    found fibfrong on medium. trying tounderstand

--- test/test_fib_frog.py
+++ test/test_fib_frog.py
@@ -23,8 +23,9 @@ The goal is to count the minimum number of jumps, so OPT(n) = min(1 + OPT(n — 
 
 https://medium.com/architectalgos/mastering-the-two-pointer-technique-a-guide-to-solving-array-problems-efficiently-71bd5a22bedc
 # Sign u for Leetcode
+# learn yield keywords
 '''
-
+import sys
 
 def generate_fib_steps(n):
     a, b = 1, 1
@@ -34,14 +35,79 @@ def generate_fib_steps(n):
         a, b = b, a + b
 
 
+def solutionA(A):
+    n = len(A) + 1
+
+    ret = [sys.maxsize] * (n + 1)
+    ret[0] = 0
+
+    for i in range(1, n + 1):
+        # so we 
+        if (i < n and A[i - 1] == 1) or (i == n):
+            ret[i] = min([ret[i - x] + 1 for x in generate_fib_steps(i)])
+            print('f')
+        else:
+            print(f'{i} is no goods')
+
+    return ret[-1] if ret[-1] < sys.maxsize else -1
+
+
 class MyTestCase(unittest.TestCase):
 
-
+    def test_sol(self):
+        A = [0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0]
+        res = solutionA(A)
     def test_newtest2(self):
         print('---- NEXT --')
-        A = [1, 1, 0, 0, 0]
-        res = solution(A)
-        self.assertEquals(2, res)
+        A = [0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0]
+
+        '''
+          # Start with -1
+          # get all the steps that can get us to ones including end
+          # then we loop thru those and do the same
+          # 
+        
+        
+        
+        '''
+
+
+
+
+        def generate_steps(idx, end, A):
+            for f in generate_fib_steps(end-idx):
+                nxt = start + f
+                if A[nxt] == 1:
+                    return nxt
+            return -1
+
+
+        # start from -1
+        # get next fib
+        # increment idx and see if A[i] ==1
+        start = -1
+        end  = len(A) + 1
+        ''' pseudocode
+            # we have the speedy fibonacci
+            # starting from idx, we get all fib numbers that from start leads to the next no
+            # if we get the algo for the first one, then we get it for everything
+        '''
+        counter = 0
+
+        for idx in range(0, len(A)+1):
+            if A[idx] == 0:
+                continue
+            else:
+                k = generate_steps(start, idx, A)
+                if k > 0:
+                    generate_steps(idx,k)
+
+
+        print(f'We needed:{counter} steps')
+
+    def test_new_fib(self):
+        for f in generate_fib_steps(100):
+            print(f)
 
     def test_single(self):
         print('---- NEXT --')
