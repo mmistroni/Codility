8711c8347845991c31169e20d6f169552aafab91
mmistroni <mmistroni@gmail.com>
2024-01-02 21:19:25 +0000

    first botched attempt

--- src/nelson_number_theory.py
+++
@@ -1,4 +0,0 @@
-#https://www.codewars.com/kata/64fb5c7a18692c0876ebbac8/train/python
-
-def play(query):
-    return 0
--- test/test_hamming_numbers.py
+++ test/test_hamming_numbers.py
@@ -2,12 +2,23 @@ import unittest
 
 from hamming_numbers import hamming
 
+# https://en.wikipedia.org/wiki/Regular_number#:~:text=In%20computer%20science%2C%20regular%20numbers%20are%20often%20called,used%20as%20a%20test%20case%20for%20functional%20programming.
+
+
+def comput_hamming(n):
+    #create 3 iterables and merge them together
+    # https://stackoverflow.com/questions/5023266/merge-join-two-generators-in-python
+
+
+   # H = 2**i * 3**j * 5 **k
+    pass
+
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(hamming(1), 1, "hamming(1) should be 1")
-        self.assertEqual(hamming(2), 2, "hamming(2) should be 2")
-        self.assertEqual(hamming(3), 3, "hamming(3) should be 3")
-        self.assertEqual(hamming(4), 4, "hamming(4) should be 4")
+        self.assertEqual(hamming(1), 1, "hamming(1) should be 1") #2 ^ 0 * 3^0 * 5^0
+        self.assertEqual(hamming(2), 2, "hamming(2) should be 2") #2 ^ 1
+        self.assertEqual(hamming(3), 3, "hamming(3) should be 3") #        3 ^ 1
+        self.assertEqual(hamming(4), 4, "hamming(4) should be 4") #2 ^ 2
         self.assertEqual(hamming(5), 5, "hamming(5) should be 5")
         self.assertEqual(hamming(6), 6, "hamming(6) should be 6")
         self.assertEqual(hamming(7), 8, "hamming(7) should be 8")
@@ -23,6 +34,65 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(hamming(17), 27, "hamming(17) should be 27")
         self.assertEqual(hamming(18), 30, "hamming(18) should be 30")
         self.assertEqual(hamming(19), 32, "hamming(19) should be 32")
+
+    def all_even(self):
+        n = 0
+        while True:
+            yield n
+            n += 2
+
+    def PowTwoGen(self, max=0):
+        n = 0
+        while n < max:
+            yield 2 ** n
+            n += 1
+
+    def merge(self, *iterables):
+        from heapq import heapify, heappop, heapreplace
+        '''Merge multiple sorted inputs into a single sorted output.
+
+        Similar to sorted(itertools.chain(*iterables)) but returns a generator,
+        does not pull the data into memory all at once, and assumes that each of
+        the input streams is already sorted (smallest to largest).
+
+        >>> list(merge([1,3,5,7], [0,2,4,8], [5,10,15,20], [], [25]))
+        [0, 1, 2, 3, 4, 5, 5, 7, 8, 10, 15, 20, 25]
+
+        '''
+        _heappop, _heapreplace, _StopIteration = heappop, heapreplace, StopIteration
+
+        h = []
+        h_append = h.append
+        for itnum, it in enumerate(map(iter, iterables)):
+            try:
+                next = it.next
+                h_append([next(), itnum, next])
+            except _StopIteration:
+                pass
+        heapify(h)
+
+        while 1:
+            try:
+                while 1:
+                    v, itnum, next = s = h[0]  # raises IndexError when h is empty
+                    yield v
+                    s[0] = next()  # raises StopIteration when exhausted
+                    _heapreplace(h, s)  # restore heap condition
+            except _StopIteration:
+                _heappop(h)  # remove empty iterator
+            except IndexError:
+                return
+
+
+    def test_itertools(self):
+        from itertools import count, accumulate
+        twos = (2 ** i for i in range(0,5))
+        twos = (3 ** i for i in range(0, 5))
+        twos = (5 ** i for i in range(0, 5))
+        for i in self.PowTwoGen(100):
+            print(f'{i}')
+
+
 
 
 if __name__ == '__main__':
