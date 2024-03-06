bdf74b4b65bae96f71fe7f3290ca6b4a93946c55
mmistroni <mmistroni@gmail.com>
2024-03-06 21:03:44 +0000

    first attempt

--- src/reverse_inside_parenthesis.py
+++ src/reverse_inside_parenthesis.py
@@ -1,3 +1,6 @@
 #https://www.codewars.com/kata/5e07b5c55654a900230f0229
 def solution(input_str):
+
+
+
     return input_str[::-1]
--- test/test_reverse_inside_parenthesis.py
+++ test/test_reverse_inside_parenthesis.py
@@ -1,9 +1,31 @@
 import unittest
 from reverse_inside_parenthesis import solution
 
+def parsestr(runner, stack, acc):
+    input = runner[0] if runner else None
+    if input == None:
+        return stack
+    if input == "(":
+        stack.append(acc)
+        stack.append(input)
+        acc = ''
+    elif input == ')':
+        stack.append(acc)
+        stack.append(input)
+        acc = ''
+    else:
+        acc += input
+    return parsestr(runner[1:], stack, acc)
+
+    # Not good enough, we need to capture all parenthesis
+
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEquals(solution("h(el)lo)"), "h(le)lo")
+        teststr  = "h(el)lo)"
+        res = parsestr(teststr, [], '')
+
+        print(res)
+        #self.assertEquals(solution("h(el)lo)"), "h(le)lo")
 
     def test_two(self):
         self.assertEquals(solution("a ((d e) c b)") , "a (b c (d e))")
