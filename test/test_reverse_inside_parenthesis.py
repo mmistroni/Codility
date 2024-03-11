01be4aed6542b5d519299a4d7ebffc3d5ad256c2
mmistroni <mmistroni@gmail.com>
2024-03-11 20:58:35 +0000

    another attempt

--- test/test_reverse_inside_parenthesis.py
+++ test/test_reverse_inside_parenthesis.py
@@ -2,32 +2,40 @@ import unittest
 from reverse_inside_parenthesis import solution
 
 def parsestr(runner, stack, acc):
+    # Not good enough as we cannot trap anything
     input = runner[0] if runner else None
     if input == None:
         return stack
     if input == "(": # not quite right, h and  lo shold be mayb sampe container?
-        stack.append(acc)
         acc = []
     elif input == ')':
         stack.append(acc)
-
-        acc = []
+        acc = None
     else:
-        acc.append(input)
+        if acc is None:
+            stack.append(input)
+        else:
+            acc.append(input)
     return parsestr(runner[1:], stack, acc)
 
     # Not good enough, we need to capture all parenthesis
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        teststr  = "h(el)lo)"
-        res = parsestr(teststr, [], [])
+        teststr  = "h(el)lo"
+        res = parsestr(teststr, [], None)
+
+        # this is ok....
 
         print(res)
         #self.assertEquals(solution("h(el)lo)"), "h(le)lo")
 
     def test_two(self):
-        self.assertEquals(solution("a ((d e) c b)") , "a (b c (d e))")
+        teststr = "a (b c (d e))"
+        res = parsestr(teststr, [], None)
+
+        print(res)
+        #self.assertEquals(solution("a ((d e) c b)") , "a (b c (d e))")
 
     def test_three(self):
         self.assertEqual(solution("one (two (three) four)"), "one (ruof (three) owt)")
