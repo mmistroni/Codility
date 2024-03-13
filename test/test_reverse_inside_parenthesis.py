15cf834e398131d56eca9216f0ab34fe50baff82
mmistroni <mmistroni@gmail.com>
2024-03-13 20:47:08 +0000

    another trial

--- test/test_reverse_inside_parenthesis.py
+++ test/test_reverse_inside_parenthesis.py
@@ -3,19 +3,28 @@ from reverse_inside_parenthesis import solution
 
 def parsestr(runner, stack, acc):
     # Not good enough as we cannot trap anything
+    # Not working yet, we need to build a stack, and we always add element to the top element of the stack
+    # whenever we encounter an open parenthesis, we create a new container and put it on stack
+    # whenever we encounter a close parenthesis, we go back to the parent. So mayb we need to keep track
+    # of the last index.
     input = runner[0] if runner else None
     if input == None:
         return stack
     if input == "(": # not quite right, h and  lo shold be mayb sampe container?
+        # we create a new accumulator
         acc = []
+
+
     elif input == ')':
+        # go back to original stack
         stack.append(acc)
         acc = None
     else:
-        if acc is None:
-            stack.append(input)
-        else:
+        if acc is not None:
             acc.append(input)
+        else:
+            stack.append(input)
+
     return parsestr(runner[1:], stack, acc)
 
     # Not good enough, we need to capture all parenthesis
