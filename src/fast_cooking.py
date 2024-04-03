e20fae09bb258019050f0582405b698a8a7aad45
mmistroni <mmistroni@gmail.com>
2024-04-03 21:21:33 +0100

    not yet htere

--- src/fast_cooking.py
+++ src/fast_cooking.py
@@ -1,8 +1,20 @@
 #https://www.codewars.com/kata/58552bdb68b034a1a80001fb
 
 def cook_pancakes(n, m):
-    # can only cook m
-    # each pk requires 2m
+    '''
+    You need to cook pancakes, but you are very hungry.
+    As known, one needs to fry a pancake one minute on each side.
+    What is the minimum time you need to cook n pancakes,
+    if you can put on the frying pan only m pancakes at a time?
+    n and m are positive integers between 1 and 1000.
+    # catch: so u need 2m x slot of pancakes as you are putting them at the same
+    # time on a pan
+    # so n pankakes takes 2m plus 2m for remainder
+
+    :param n:
+    :param m:
+    :return:
+    '''
 
     if n < m:
         nums = 1
@@ -10,4 +22,3 @@ def cook_pancakes(n, m):
         nums = n // m
 
     return 2 * nums
-
--- test/test_cook_pancakes.py
+++ test/test_cook_pancakes.py
@@ -12,6 +12,13 @@ class MyTestCase(unittest.TestCase):
     def test_something3(self):
         self.assertEquals(cook_pancakes(4, 2), 4)
 
+    def test_something4(self):
+        self.assertEquals(cook_pancakes(3, 2), 3)
+
+    def test_something5(self):
+        self.assertEquals(cook_pancakes(4, 3), 3)
+
+
 
 
 if __name__ == '__main__':
