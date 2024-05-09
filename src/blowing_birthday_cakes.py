a64cb8b67b8150d0aead520752705e27b81d8d82
mmistroni <mmistroni@gmail.com>
2024-05-09 20:54:51 +0100

    first run(not working)

--- src/blowing_birthday_cakes.py
+++ src/blowing_birthday_cakes.py
@@ -1,3 +1,31 @@
 #https://www.codewars.com/kata/6630da20f925eb3007c5a498
+
+def _check(lst) :
+    return all([(i == 0) for i in lst])
+
+def _decrease(lst):
+    # not right
+    if 0 in lst:
+        idx = lst.index(0)
+    else:
+        idx = -1
+    bound1, bound2 = idx +1, idx + 4
+
+    #need better 
+    new_array = [item-1 if bound1 <= idx < bound2 and item > 0 else item for idx, item in enumerate(lst) ]
+
+    return new_array
+
+
 def blow_candles(st):
-    pass
+    toint = [int(s) for s in st]
+
+    st = toint
+    count = 0
+
+    while not _check(st):
+        count +=1
+        st = _decrease(st)
+
+    return count
+
--- test/test_blowing_birthday_cakes.py
+++ test/test_blowing_birthday_cakes.py
@@ -1,19 +1,29 @@
 import unittest
 from blowing_birthday_cakes import blow_candles
-
+import  time
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        def basic_test_cases():
-            tests = [
-                ["1321", 3],
-                ["0323456", 9],
-                ["2113", 5],
-                ["1110", 1],
-                ["121", 2]
-            ]
-            for st, exp in tests:
-                self.assertEqual(blow_candles(st), exp)
 
+    def test_simple(self):
+        candles = "1321"
+
+        self.assertEqual(blow_candles(candles), 3)
+
+
+    def test_something(self):
+        tests = [
+            ["1321", 3],
+            ["0323456", 9],
+            ["2113", 5],
+            ["1110", 1],
+            ["121", 2]
+        ]
+
+        for st, exp in tests:
+            start = time.time()
+
+            self.assertEqual(blow_candles(st), exp)
+            end = time.time()
+            print(f'Took {start - end}')
 
 if __name__ == '__main__':
     unittest.main()
