6ad266ef94c5b374c9ae008dc257c71c88e04ae2
mmistroni <mmistroni@gmail.com>
2024-05-29 21:38:23 +0100

    added relatively prime numbrs test

---
+++ src/relative_prime_numbers.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/56b0f5f84de0afafce00004e/train/python
+
+
+def relatively_prime (n, l):
+    return []
---
+++ test/test_relative_prime_numbers.py
@@ -0,0 +1,20 @@
+import unittest
+from relative_prime_numbers import relatively_prime
+
+class MyTestCase(unittest.TestCase):
+
+    def test_something1(self):
+        self.assertEquals(relatively_prime(8, [1, 2, 3, 4, 5, 6, 7]), [1, 3, 5, 7])
+
+    def test_something2(self):
+        self.assertEquals(relatively_prime(15, [72, 27, 32, 61, 77, 11, 40]), [32, 61, 77, 11])
+
+    def test_something3(self):
+        self.assertEquals(relatively_prime(210, [15, 100, 2222222, 6, 4, 12369, 99]), [])
+
+
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
