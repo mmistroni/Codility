13f3cf353ad3fc27cce494c38f50620fc9556ffa
mmistroni <mmistroni@gmail.com>
2024-06-17 20:52:18 +0100

    minimum bonus

--- src/minimum_bonus.py
+++ src/minimum_bonus.py
@@ -1,1 +1,24 @@
 # https://www.codewars.com/kata/595ef0c7458ad5facc000019
+
+
+def calculate_bonus(inarray):
+    bon_array = []
+    for idx in range(0, len(inarray)):
+        if idx == 0:
+            # first one, default 1
+            bon_array.append(1)
+        else:
+            if inarray[idx] > inarray[idx - 1]:
+                latest = bon_array[-1]
+                bon_array.append(latest + 1)
+            else:
+                bon_array.append(1)
+
+    return bon_array
+
+
+def minimum_bonus(scores):
+    allb = calculate_bonus(scores)
+    return sum(allb)
+
+
---
+++ test/test_minimum_bonus.py
@@ -0,0 +1,86 @@
+import unittest
+
+from minimum_bonus import minimum_bonus
+
+
+sample_test_cases = [
+    #                     scores      expected
+    ([20,30,10,30,40,10,20,30,40,30],   20),
+    (                   [10, 20, 30],    6),
+    (                   [30, 20, 10],    6),
+    (               [30, 20, 20, 10],    6),
+    (               [10, 20, 20, 30],    6),
+    (               [20, 20, 20, 20],    4),
+    (       [20, 30, 40, 30, 20, 10],   13),
+]
+
+class MyTestCase(unittest.TestCase):
+
+
+    def calculate_bonus(self, inarray):
+        bon_array = []
+        for idx in range(0, len(inarray)):
+            if idx == 0:
+                # first one, default 1
+                bon_array.append(1)
+            else:
+                if inarray[idx] > inarray[idx-1]:
+                    latest = bon_array[-1]
+                    bon_array.append(latest + 1)
+                else:
+                    bon_array.append(1)
+
+        return bon_array
+
+
+    def xtester1(self):
+
+        tst = [20,30,10,30,40,10,20,30,40,30]
+
+        res = self.calculate_bonus(tst)
+
+        print(f'Array is:{res}. sum is:{sum(res)}')
+
+
+
+    def test_1(self):
+        self.assertEqual(minimum_bonus([20,30,10,30,40,10,20,30,40,30]),20)
+
+    def test_1(self):
+        self.assertEqual(minimum_bonus([10, 20, 30]), 6)
+
+    def test_2(self):
+        self.assertEqual(minimum_bonus([30, 20, 10]), 6)
+    def test_3(self):
+        self.assertEqual(minimum_bonus([30, 20, 20, 10]), 6)
+                                        
+    def test_4(self):
+
+        self.assertEqual(minimum_bonus([10, 20, 20, 30]), 6)
+
+    def test_5(self):
+        self.assertEqual(minimum_bonus([20, 20, 20, 20]), 4)
+
+    def test_6(self):
+        self.assertEqual(minimum_bonus([20, 30, 40, 30, 20, 10]), 13)
+                                         2   3   4   1   1   1
+    '''
+    20,30,10,30,40,10,20,30,40,30
+    
+    1   2  1  2  3  1 2  3  4   1    
+    
+    
+    
+    
+    1
+    2
+    1
+    2
+    3
+    
+    '''
+
+
+
+if __name__ == '__main__':
+    unittest.main()
