004ad90f57a992f6fe5982a533131c704e9ac33f
mmistroni <mmistroni@gmail.com>
2024-07-25 21:06:45 +0100

    another try

--- test/test_minimum_bonus.py
+++ test/test_minimum_bonus.py
@@ -14,6 +14,33 @@ sample_test_cases = [
     (       [20, 30, 40, 30, 20, 10],   13),
 ]
 
+
+def _calculate_bonus(inarray):
+    bon_array = []
+    for idx in range(0, len(inarray)):
+        if idx == 0:
+            if inarray[idx] < inarray[idx+1]:
+            # first one, default 1
+                bon_array.append(1)
+        else:
+            if inarray[idx] > inarray[idx - 1]:
+                latest = bon_array[-1]
+                bon_array.append(latest + 1)
+            else:
+                bon_array.append(1)
+
+    return bon_array
+
+
+def _minimum_bonus(scores):
+    allb = _calculate_bonus(scores)
+
+
+
+    return sum(allb)
+
+
+
 class MyTestCase(unittest.TestCase):
 
 
@@ -32,51 +59,59 @@ class MyTestCase(unittest.TestCase):
 
         return bon_array
 
-
     def _rule_of_10(self, tst):
         tens_idx = [idx  for idx, i in enumerate(tst) if i == 10]
         bonus_pack = [0] * len(tst)
         for idx in tens_idx:
             bonus_pack[idx] = 1
+            if idx > 0:
+                bonus_pack[idx-1] = 2
+            if idx < len(tst)-1:
+                bonus_pack[idx+1] = 2
 
-        def fill_zeros(tst):
-            cp = tst.copy()
-            for idx in range(0, len(cp)):
-                if cp[idx] == 0:
-                    cp[idx] = 1
+        print(f'Tst is:{tst}')
+        print(f'Bonus is:{bonus_pack}')
 
-            return cp
-
-
-        # New strategy. we start from 0. and check if the next one is a 10, ..
-        # not convinced it'll work though
-        for idx, item in enumerate(tst):
-            if item == 10:
-                continue
-            if idx == 0:
-                if tst[idx] < tst[idx+1]:
-                    if tst[idx+1] !=0:
-                        bonus_pack[idx] = 1
-
-                if bonus_pack[idx] > 0:
-                    continue
-                else:
-                    if item < tst[idx] + 1:
-                        bonus_pack[idx] = 1
-            else:
-                if tst[idx] > tst[idx-1]:
-                    bonus_pack[idx] = bonus_pack[idx-1] + 1
-            # if there are any zeros, they should be ones
-        print(bonus_pack)
-
-        return fill_zeros(bonus_pack)
+        # once we fill the 10s we start to fill the right
+        # then the left
+        # then we have to fill the array till all zeros are gone
 
 
     def test_rule_of_10(self):
-        tst = [20, 30, 40, 30, 20, 10]#[20, 30, 10, 30, 40, 10, 20, 30, 40, 30]
+        tst = [20, 30, 40, 30, 20, 10]# 1 2 3  2 1 1
+        # working
+        # [20, 30, 10, 30, 40, 10, 20, 30, 40, 30]# 6
         res = self._rule_of_10(tst)
-        print(res)
-        print(sum(res))
+        # Next stop: start from the first item you can populate on the right
+        #print(res)
+        #print(sum(res))
+        #self.assertEquals(13, sum(res))
+
+    def test_rule_of_10_2(self):
+        tst = [10, 20, 30]
+                           # working
+        # [20, 30, 10, 30, 40, 10, 20, 30, 40, 30]# 6
+        res = self._rule_of_10(tst)
+        # Next stop: start from the first item you can populate on the right
+        #print(res)
+        #print(sum(res))
+        #self.assertEquals(13, sum(res))
+
+    def test_rule_of_10_3(self):
+        tst = [30, 20, 10]
+        self._rule_of_10(tst)
+
+    def test_rule_of_10_4(self):
+        tst =  [10, 20, 20, 30]
+        self._rule_of_10(tst)
+
+    def test_rule_of_10_5(self):
+        tst =  [20, 20, 20, 20]
+        self._rule_of_10(tst)
+
+    def test_rule_of_10_5(self):
+        tst =  [20, 30, 40, 30, 20, 10]
+        self._rule_of_10(tst)
 
 
     def xtester1(self):
@@ -90,13 +125,17 @@ class MyTestCase(unittest.TestCase):
 
 
     def test_1(self):
-        self.assertEqual(minimum_bonus([20,30,10,30,40,10,20,30,40,30]),20)
+        self.assertEqual(_minimum_bonus([20,30,10,30,40,10,20,30,40,30]),20)
 
     def test_1(self):
-        self.assertEqual(minimum_bonus([10, 20, 30]), 6)
+        self.assertEqual(_minimum_bonus([10, 20, 30]), 6)
 
     def test_2(self):
-        self.assertEqual(minimum_bonus([30, 20, 10]), 6)
+        self.assertEqual(_minimum_bonus([30, 20, 10]), 6)
+
+
+
+
     def test_3(self):
         self.assertEqual(minimum_bonus([30, 20, 20, 10]), 6)
 
@@ -109,6 +148,8 @@ class MyTestCase(unittest.TestCase):
 
     def test_6(self):
         self.assertEqual(minimum_bonus([20, 30, 40, 30, 20, 10]), 13)
+        #                                 1   2   4    3    2    1
+
 
 
 
