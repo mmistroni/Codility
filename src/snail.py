c75e06fcb514153598644d6d9908a10c97d655fd
mmistroni <mmistroni@gmail.com>
2024-09-02 20:57:00 +0100

    trying with a new snail run

---
+++ src/snail.py
@@ -0,0 +1,32 @@
+#https://www.codewars.com/kata/66cdc6ab9e7a9f009e0ca8f6Problem Description:
+'''
+A snail is crawling along a rubber band that has an initial length of x units. The snail moves at a constant speed of y units per minute. As the snail crawls from the left end of the rubber band to the right end, the rubber band increases in length from the right side every minute, adding z units to its length.
+
+The question is: Will the snail be able to reach the right end of the rubber band within 1 year?
+
+Parameters:
+
+x: Initial length of the rubber band (in units), where 0.01 ≤ x ≤ 1,000,000.
+y: Speed of the snail (in units per minute), where 0.01 ≤ y ≤ 1,000,000.
+z: Amount by which the rubber band increases in length every minute (in units), where 0.01 ≤ z ≤ 1,000,000.
+'''
+
+
+year_mins = 60 * 24 * 365
+
+
+def can_crawl(x, y, z ):
+
+    # Not working. we need to calculate this to avoid looping.
+
+
+    snail_tracker = 0
+    for i in range(0, year_mins):
+        snail_tracker += y
+        x += z
+        if snail_tracker >= x:
+            return True
+
+        print(f':Snail Track:({snail_tracker})|Track:{x}')
+    return False
+
---
+++ test/test_snail.py
@@ -0,0 +1,29 @@
+import unittest
+from snail import can_crawl
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_example1(self):
+        rubber_band = 10
+        snail_speed = 2  # 2 unit minute
+        rate_of_rubber_band = 1 # 1 unit minute
+        self.assertEqual(True, can_crawl(rubber_band, snail_speed, rate_of_rubber_band))
+
+    def test_example2(self):
+        rubber_band = 100
+        snail_speed = 1  # 2 unit minute
+        rate_of_rubber_band = 2  # 1 unit minute
+        self.assertEqual(False, can_crawl(rubber_band, snail_speed, rate_of_rubber_band))
+
+
+    def test_example3(self):
+        rubber_band = 100000
+        snail_speed = 0.1  # 2 unit minute
+        rate_of_rubber_band = 0.05  # 1 unit minute
+        self.assertEqual(False, can_crawl(rubber_band, snail_speed, rate_of_rubber_band))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
