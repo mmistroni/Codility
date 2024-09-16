f44c80c0fb47dc2abf137b1d84d03d0072b97448
mmistroni <mmistroni@gmail.com>
2024-09-16 21:15:52 +0100

    another one done

--- src/like_or_dislike.py
+++ src/like_or_dislike.py
@@ -1,7 +1,26 @@
 #https://www.codewars.com/kata/62ad72443809a4006998218a
 
+from enum import Enum
+#from preloaded import Like, Dislike, Nothing
+from functools import reduce
 
-from preloaded import Like, Dislike, Nothing
+
+class Test(Enum):
+    Like  = 1,
+    Dislike = 2.
+    Nothing = 3
+
+def choose(last, current):
+    if last == current:
+        return Test.Nothing
+    else:
+        return current
+
 
 def like_or_dislike(lst):
-    return Like or Dislike or Nothing
+    if not lst:
+        return Test.Nothing
+
+    res = reduce(lambda acc, item: choose(acc, item), lst[1:], lst[0] )
+
+    return res
--- test/test_like_or_dislike.py
+++ test/test_like_or_dislike.py
@@ -1,18 +1,21 @@
-import codewars_test as test
-from like_or_dislike import  import like_or_dislike
-from preloaded import Like, Dislike, Nothing
 
-@test.describe("Fixed tests")
-def fixed_tests():
-    @test.it('Basic Tests')
-    def _():
-        test.assert_equals(like_or_dislike([Dislike]), Dislike, repr([Dislike]))
-        test.assert_equals(like_or_dislike([Like, Like]), Nothing, repr([Like, Like]))
-        test.assert_equals(like_or_dislike([Dislike, Dislike]), Nothing, repr([Dislike, Dislike]))
-        test.assert_equals(like_or_dislike([Like, Like, Like]), Like, repr([Like, Like, Like]))
-        test.assert_equals(like_or_dislike([Like, Dislike]), Dislike, repr([Like, Dislike]))
-        test.assert_equals(like_or_dislike([Dislike, Like]), Like, repr([Dislike, Like]))
-        test.assert_equals(like_or_dislike([Like, Dislike, Dislike]), Nothing, repr([Like, Dislike, Dislike]))
-        test.assert_equals(like_or_dislike([Dislike, Like, Dislike]), Dislike, repr([Dislike, Like, Dislike]))
-        test.assert_equals(like_or_dislike([Like, Like, Dislike, Like, Like, Like, Like, Dislike]), Dislike, repr([Like, Like, Dislike, Like, Like, Like, Like, Dislike]))
-        test.assert_equals(like_or_dislike([]), Nothing, repr([]))
+from like_or_dislike import  like_or_dislike, Test
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_fixed_tests(self):
+        self.assertEquals(like_or_dislike([Test.Dislike]), Test.Dislike)
+        self.assertEquals(like_or_dislike([Test.Like, Test.Like]), Test.Nothing)
+        self.assertEquals(like_or_dislike([Test.Dislike, Test.Dislike]), Test.Nothing)
+        self.assertEquals(like_or_dislike([Test.Like, Test.Like, Test.Like]), Test.Like)
+        self.assertEquals(like_or_dislike([Test.Like, Test.Dislike]), Test.Dislike)
+        self.assertEquals(like_or_dislike([Test.Dislike, Test.Like]), Test.Like)
+        self.assertEquals(like_or_dislike([Test.Like, Test.Dislike, Test.Dislike]), Test.Nothing)
+        self.assertEquals(like_or_dislike([Test.Dislike, Test.Like, Test.Dislike]), Test.Dislike)
+        self.assertEquals(like_or_dislike([Test.Like, Test.Like, Test.Dislike,
+                                           Test.Like, Test.Like, Test.Like, Test.Like, Test.Dislike]),
+                                            Test.Dislike)
+        self.assertEquals(like_or_dislike([]), Test.Nothing)
+        
