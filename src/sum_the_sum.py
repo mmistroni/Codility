7f7322228180ca2b445d85d7e80c81e2d8b93c8d
mmistroni <mmistroni@gmail.com>
2024-09-26 20:56:24 +0100

    improved but not there

--- src/sum_the_sum.py
+++ src/sum_the_sum.py
@@ -1,15 +1,27 @@
 #https://www.codewars.com/kata/60d2325592157c0019ee78ed
 
-def gauss(n):
-    return (n * (n + 1)) /2
+def gauss(n) -> int:
+    return (n * (n + 1)) / 2
 
 
-def S(n):
+def S(n) -> int:
     return gauss(n)
 
-def Z(n):
-    return sum([S(i) for i in range(1, n + 1)])
+def Z(n) -> int:
 
+    '''
+    holder = 0
+    for i in range(1, n + 1):
+        holder += S(i)
+
+    return holder
+    '''
+    result = [0]
+    # Gauss sum for 0 is 0
+    res = 0
+    for i in range(1, n + 1):
+        result.append(result[-1] + i)
+    return sum(result)
 
 def sum_of_sums(n):
     zetas = Z(n)
--- test/test_sum_the_sum.py
+++ test/test_sum_the_sum.py
@@ -1,5 +1,6 @@
 from sum_the_sum import sum_of_sums
 import unittest
+import time
 
 class MyTestCase(unittest.TestCase):
 
@@ -12,7 +13,20 @@ class MyTestCase(unittest.TestCase):
     def test3(self):
         self.assertEquals(14740530850, sum_of_sums(100))
 
+    def test4(self):
 
+
+        tester = 5* (10 ** 4)
+        res = sum_of_sums(tester)
+        print(f'Sum of sum ={res}')
+
+    def test5(self):
+        tester = 9 * 10 ** 8
+
+        start = time.time()
+        res = sum_of_sums(tester)
+        end = time.time()
+        print(f'Res={res} Calculated in {end-start} seconds')
 
 if __name__ == '__main__':
     unittest.main()
