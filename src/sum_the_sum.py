7d9875b5f894d79c8339521ae794047a346f3f45
mmistroni <mmistroni@gmail.com>
2024-10-01 20:21:59 +0100

    solved sum of sums

--- src/sum_the_sum.py
+++ src/sum_the_sum.py
@@ -1,7 +1,7 @@
 #https://www.codewars.com/kata/60d2325592157c0019ee78ed
 
 def gauss(n) -> int:
-    return (n * (n + 1)) / 2
+    return (n * (n + 1)) // 2
 
 
 def S(n) -> int:
@@ -16,12 +16,7 @@ def Z(n) -> int:
 
     return holder
     '''
-    result = [0]
-    # Gauss sum for 0 is 0
-    res = 0
-    for i in range(1, n + 1):
-        result.append(result[-1] + i)
-    return sum(result)
+    return (n * (n + 1) * (n + 2)) // (2 * 3)
 
 def sum_of_sums(n):
     zetas = Z(n)
