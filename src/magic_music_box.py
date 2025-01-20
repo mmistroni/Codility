2b79227de5eb53fe9b9eac4cff9aa7f747cd20bf
mmistroni <mmistroni@gmail.com>
2025-01-20 20:30:51 +0000

    not yet there, need to handle more usecases

--- src/magic_music_box.py
+++ src/magic_music_box.py
@@ -1,5 +1,6 @@
 #https://www.codewars.com/kata/6710e54f8ef071fe99eebd07/train/python
 import re
+
 from collections import defaultdict
 NOTES = ["DO", "RE", "MI", "FA", "SOL", "LA", "SI"]
 
@@ -16,32 +17,45 @@ def play(words):
     dupes = []
 
     looping = 0
+    ddict = defaultdict(list)
+
     while True:
         # we exit either when array is empty or when we found all the notes
         # if a note is missing we should break and return what we have
         if not words:
             break
+        lookup_idx = 0
+        seen = []
         for note in patterns:
             note_found = False
-            for idx, word in enumerate(words):
+            for idx, word in enumerate(words[lookup_idx:]):
                 if note in word:
-                    if word not in found:
-                        found.append(word)
+                    ddict[note].append(1)
+                    if word not in seen:
+                        nidx = notes_dict.get(note)
+                        if len(ddict[note]) > 1:
+                            nidx += 7
+                        found.append((word,nidx))
+                        seen.append(word)
                     else:
                         dupes.append(word)
                     note_found = True
-                    words.pop(idx)   
+                    words.remove(word)   
             if not note_found:#
                 return found  
             else:
+                lookup_idx = idx if idx < len(words)-1 else 0
                 continue     
-        
     return found
-
 
 def magic_music_box(words):
 
     if not words:
         return []
-    return play(words)
+    found =  play(words)
+    sorted_l = sorted(found, key=lambda x: (x[1]))
+
+    res = [t[0] for t in sorted_l]    
+    return res
+
     
--- test/test_magic_music_box.py
+++ test/test_magic_music_box.py
@@ -33,45 +33,6 @@ class MyTestCase(unittest.TestCase):
         res = magic_music_box(words)
         self.assertEqual(expected, res)
 
-    def test_sample2(self):
-        #words = ['DOWN', 'PLANE', 'AMIDST', 'REPTILE', 'SOFA', 'SOLAR', 'SILENCE', 'DOWN', 'MARKDOWN']
-        #expected = ['DOWN', 'REPTILE', 'AMIDST', 'SOFA', 'SOLAR', 'PLANE', 'SILENCE', 'MARKDOWN']  # expected
-        words = []
-        expected = []
-        
-        patterns = "DO|RE|MI|FA|SOL|LA|SI".split("|")
-        
-        found = []
-        dupes = []
-
-        while True:
-
-            # we exit either when array is empty or when we found all the notes
-            if not words:
-                break
-            # we need to find out the second condition, when the array is not empty    
-            for note in patterns:
-                note_found = False
-                logging.info(f'Checking:{note} ')
-                # if we cannot find the note in any of the workds
-                for idx, word in enumerate(words):
-                    if note in word:
-                        if word not in found:
-                            found.append(word)
-                        else:
-                            dupes.append(word)
-                        note_found = True
-                        words.pop(idx)   
-                if not note_found:
-                    return found         
-            if not found:
-                break
-        print(f'We found:\n {found}')
-        
-        self.assertEqual(expected, found)
-
-    
-    
     def test_four(self):
         res = magic_music_box(['DOWN', 'AMIDST', 'SOFA', 'FACTION'])
         self.assertEqual(res, ['DOWN'])
@@ -86,25 +47,25 @@ class MyTestCase(unittest.TestCase):
     def test_duplicates(self):
         res = magic_music_box(['DOOR', 'DOOR', 'REPTILE', 'REPTILE', 'SIMILAR', 'SIMILAR', 'SOFA', 'SOFA', 'DISSOLVED', 'DISSOLVED', 'LAPTOP', 'LAPTOP', 'RESIST', 'RESIST'])
         expected =  ['DOOR', 'REPTILE', 'SIMILAR', 'SOFA', 'DISSOLVED', 'LAPTOP', 'RESIST']
-        self.assertEquals(expected, res)
+        self.assertEqual(expected, res)
 
     def test_otherusecase(self):
         res = magic_music_box(['RECORD', 'LAPTOP', 'DOWN', 'CAR', 'MOUSE', 'DINNER', 'CORRECT'])
-        self.assertEquals(['DOWN', 'CORRECT'], res)
+        self.assertEqual(['DOWN', 'CORRECT'], res)
 
     def test_otherusecases2(self):
         res = magic_music_box(['RECORD', 'COMIC', 'LAPTOP', 'DOWN', 'CAR', 'MOUSE', 'DINNER', 'CORRECT'])
-        self.assertEquals( ['DOWN', 'CORRECT', 'COMIC'], res)
+        self.assertEqual( ['DOWN', 'CORRECT', 'COMIC'], res)
 
     def test_otherusecases3(self):
-        res = magic_music_box(['RECORD', 'COMIC', 'LAPTOP', 'DOWN', 'CAR', 'FAMILY', 'MOUSE', 'DINNER', 'CORRECT']): 
-        self.assetEquals( ['DOWN', 'CORRECT', 'COMIC', 'FAMILY'], res)
+        res = magic_music_box(['RECORD', 'COMIC', 'LAPTOP', 'DOWN', 'CAR', 'FAMILY', 'MOUSE', 'DINNER', 'CORRECT'])
+        self.assetEqual( ['DOWN', 'CORRECT', 'COMIC', 'FAMILY'], res)
 
     def test_otherusecases4(self):
         res = magic_music_box(['RECORD', 'COMIC', 'DOWN', 'CAR', 'FAMILY', 'MOUSE', 'DISSOLVED', 'DINNER', 'CORRECT'])
-        self.assertEquals(['DOWN', 'CORRECT', 'COMIC', 'FAMILY', 'DISSOLVED'], res)
+        self.assertEqual(['DOWN', 'CORRECT', 'COMIC', 'FAMILY', 'DISSOLVED'], res)
 
     def test_otherusecases5(self):
         res = magic_music_box(['RECORD', 'COMIC', 'LAPTOP', 'SYLLABLE', 'DOWN', 'CAR', 'FAMILY', 'MOUSE', 'DISSOLVED', 'DINNER', 'CORRECT'])
-        self.assertEquals( ['DOWN', 'CORRECT', 'COMIC', 'FAMILY', 'DISSOLVED', 'LAPTOP'], res)
+        self.assertEqual( ['DOWN', 'CORRECT', 'COMIC', 'FAMILY', 'DISSOLVED', 'LAPTOP'], res)
 
