8d6a9778203cb7c0aca7ea80ee4465bba31cbf74
mmistroni <mmistroni@gmail.com>
2025-02-25 20:13:51 +0000

    update

--- src/bouncing_balls.py
+++ src/bouncing_balls.py
@@ -3,4 +3,12 @@
 
 def bouncing_ball(h, bounce, window):
     # your code
+    # we need to compare bounce vs height of window
+    if h > 0 and 0 < bounce < 1 and window < h:
+        start = 1
+        bounce_height = h * bounce
+        if window > bounce_height:
+            return start
+        else:
+            return start + 2
     return -1
