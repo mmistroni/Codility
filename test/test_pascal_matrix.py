60fff4819218935c39483e22a59abd2203fd9a4f
mmistroni <mmistroni@gmail.com>
2025-03-24 21:21:58 +0000

    another attempt, we are on our way

--- test/test_pascal_matrix.py
+++ test/test_pascal_matrix.py
@@ -1,6 +1,68 @@
 from src.pascal_matrix import pascal_matrix
 import unittest
 
+
+def modified_pascal_matrix(n):
+    """
+    Generates a modified Pascal's triangle with zeros inserted, for generic Pascal matrix.
+
+    Args:
+      n: The number of rows to generate.
+
+    Returns:
+      A list of lists representing the modified Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+    if n >= 1:
+        triangle.append([0, 1, 0])
+    if n >= 2:
+        triangle.append([1, 0, 1])
+
+    for i in range(2, n):
+        prev_row = triangle[i - 1]
+        new_row = []
+        pascal_row = [1] * (i + 1)
+
+        for j in range(1, i):
+            # Boundary checks to prevent IndexError
+            left = prev_row[2 * (j - 1) + 1] if 2 * (j - 1) + 1 < len(prev_row) else 0
+            right = prev_row[2 * j + 1] if 2 * j + 1 < len(prev_row) else 0
+            pascal_row[j] = left + right
+
+        for val in pascal_row:
+            new_row.append(val)
+            new_row.append(0)
+
+        new_row.pop()
+        triangle.append(new_row)
+
+    return triangle
+
+def pascal_triangle(n):
+    """
+    Calculates Pascal's triangle up to row n.
+
+    Args:
+        n: The number of rows to generate.
+
+    Returns:
+        A list of lists representing Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)  # Initialize row with 1s
+        if i > 1:
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle
+
 class MyTestCase(unittest.TestCase):
     sample_test_cases = [
         ('Simple example',
@@ -95,42 +157,114 @@ class MyTestCase(unittest.TestCase):
 
         return newholder
 
-    def insert_zeros_respecting_length(self, matrix):
-        """Inserts zeros between non-zero elements, respecting the original row lengths."""
-        new_matrix = []
-        for row in matrix:
+
+    def modified_pascal_matrix(self, n):
+        """
+        Generates a modified Pascal's triangle with zeros inserted, for generic Pascal matrix.
+
+        Args:
+            n: The number of rows to generate.
+
+        Returns:
+            A list of lists representing the modified Pascal's triangle.
+        """
+        if n <= 0:
+            return []
+
+        triangle = []
+
+        for i in range(n):
+            if i == 0:
+                triangle.append([0, 1, 0])
+                continue
+
+            prev_row = triangle[i - 1]
             new_row = []
-            zero_count = 0  # Count of zeros inserted
-            for i in range(len(row)):
-                if row[i] != 0:
-                    new_row.append(row[i])
-                    if i < len(row) - 1 and row[i + 1] != 0:
-                        if len(new_row) < len(row):  # Check if there's space
-                            new_row.append(0)
-                            zero_count += 1
-                else:
-                    if len(new_row) < len(row): # only append if there is space.
-                        new_row.append(0)
+            pascal_row = [1] * (i + 1)
 
-            # Pad the row with zeros if needed to match the original length
-            while len(new_row) < len(row):
+            if i > 1:
+                for j in range(1, i):
+                    # Calculate Pascal's triangle values from the *previous* modified row
+                    pascal_row[j] = prev_row[2 * (j - 1) + 1] + prev_row[2 * j + 1]
+
+            for val in pascal_row:
+                new_row.append(val)
                 new_row.append(0)
+            
+            new_row.pop() #remove the last zero.
+            triangle.append(new_row)
 
-            new_matrix.append(new_row)
-        return new_matrix    
+        return triangle
+
+
+
+
 
     def test_triangle(self):
-        n = 3
+        #print(modified_pascal_matrix(3))
+        #print(modified_pascal_matrix(5))
+        #print(modified_pascal_matrix(9))
+        # we start fromm this to figure out how many element sin last row
+        # and from this we can generate the triangle
+        for i in [3,5,9]:
+            print(f'-------------------- {i} ---------------------')
+            print(self.count_nonzero_elements(i))
 
-        res = self.build_pascal_triangle(n)
-        print(res)
-        from pprint import pprint
-        refactored = self.insert_zeros_respecting_length(res)
+
+
+        #res = self.modified_pascal_matrix(n)
+        #print(res)
+        
+        ''' 
+            we need to figure out the link between pascal triangle and pascal matrix
+            2^n.
+
+
+            pascalMatrix(3) =
+                    [[0,1,0], 
+                    [1,0,1]]
+                        so this is the matrix for pascal(2)
+            pascalMatrix(5) =
+
+                [[0,0,1,0,0], 
+                [0,1,0,1,0], 
+                [1,0,2,0,1]]
+                this is the matrix for pascal(3)
+
+                
+            pasalMatrix(9)
+                [[0,0,0,0,1,0,0,0,0],
+                [0,0,0,1,0,1,0,0,0],
+                [0,0,1,0,2,0,1,0,0],
+                [0,1,0,3,0,3,0,1,0],
+                [1,0,4,0,6,0,4,0,1]]
+                this is the matrix for pascal(5)                            
+        
+        
+        '''
+
+
+        #from pprint import pprint
+        #refactored = self.insert_zeros_respecting_length(res)
         #refactored = self.to_matrix(n, res)
-        for item in refactored:
-            print(item)
+        #for item in refactored:
+        #    print(item)
 
+    def count_nonzero_elements(self, last_row_elements):
+        """
+        Calculates the number of non-zero elements in a row where zeros are inserted 
+        between every other number.
 
+        Args:
+            last_row_elements: The total number of elements in the last row.
+
+        Returns:
+            The number of non-zero elements.
+        """
+        if last_row_elements <= 0:
+            return 0
+
+        return (last_row_elements + 1) // 2
 
 
 
