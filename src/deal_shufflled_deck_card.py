e7deef860c76452661fbb792fa481417d7968ca6
mmistroni <mmistroni@gmail.com>
2025-05-20 20:22:42 +0000

    sorted another problem

--- src/deal_shufflled_deck_card.py
+++ src/deal_shufflled_deck_card.py
@@ -1,3 +1,13 @@
+#https://www.codewars.com/kata/5810ad962b321bac8f000178/train/python
+import random 
+suits = ['H', 'C', 'D', 'S']
+
+cards = range(1, 14)
+
+DECK = [f"{suit} {value}" for suit in suits for value in cards]
+
+
 def shuffled_deck():
     # your smart code here
-    return []
+    return random.sample(DECK, len(DECK))
+    
--- test/test_deal_shuffled_deck_card.py
+++ test/test_deal_shuffled_deck_card.py
@@ -1,4 +1,4 @@
-from src.deal_shufflled_deck_card import shuffled_deck
+from src.deal_shufflled_deck_card import shuffled_deck, suits, DECK
 import unittest
 import math
 
@@ -8,4 +8,8 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(len(set(deck1)), 52, "deck #1 should consist of 52 cards")
         self.assertEqual(len(set(deck2)), 52, "deck #2 should consist of 52 cards")
         self.assertEqual(sorted(deck1), sorted(deck2), "decks should contain the same cards")
-        self.assert_not_equals(deck1, deck2, "decks should be shuffled")
+        self.assertNotEqual(deck1, deck2, "decks should be shuffled")
+
+    def test2(self):
+        from pprint import pprint
+        pprint(DECK)
