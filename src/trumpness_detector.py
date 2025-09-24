afce7906636211378a0445edfc872757bc7a7fa2
mmistroni <mmistroni@gmail.com>
2025-09-24 20:01:51 +0000

    sync

--- src/trumpness_detector.py
+++ src/trumpness_detector.py
@@ -1,4 +1,4 @@
 #https://www.codewars.com/kata/57829376a1b8d576640000d6/train/python
 
-def solution(w):
+def trump_detector(w):
     return 0
--- test/test_trump_detector.py
+++ test/test_trump_detector.py
@@ -1,26 +1,23 @@
 import unittest
 import itertools
+
 from src.trumpness_detector import trump_detector
-
 class MyTestCase(unittest.TestCase):
-
-    def test_one(self):
-        self.assertEqual(0, trump_detector("I will build a huge wall"))
     
+    def test_one(self):
+        self.assertEquals(trump_detector("I will build a huge wall"), 0)
+        
     def test_two(self):
+        self.assertEquals(4, trump_detector("HUUUUUGEEEE WAAAAAALL"))
+    
+    
+    def test_something(self):
+        sentence = "HUUUUUGEEEE WAAAAAALL"
 
-        self.assertEqual(4, trump_detector("HUUUUUGEEEE WAAAAAALL"))
-    def test_three(self):
-        self.assertEquals( 2.5, trump_detector("MEXICAAAAAAAANS GOOOO HOOOMEEEE"))
-    def test_four(self):
-        self.assertEquals(1.89, trump_detector("America NUUUUUKEEEE Oooobaaaamaaaaa"))
-    def test_five(self):
-        self.assertEquals(1.56, 
-                          trump_detector("listen migrants: IIII KIIIDD YOOOUUU NOOOOOOTTT"))
-    def test_mytester(self):
-        sentence = 'HUUUUUGEEEE WAAAAAALL'
+        vowels = ['a', 'e', 'i', 'o', 'u']
 
-        letters = [char for char in sentence if char in ['A', 'E', 'I', 'O', 'U']]
+        # 1. Filter out non-alphabetic characters and convert to lowercase
+        letters = [char for char in sentence.lower() char in vowels]
 
         # 2. Sort the list of letters
         sorted_letters = sorted(letters)
@@ -29,10 +26,11 @@ class MyTestCase(unittest.TestCase):
         letter_counts = {}
         for key, group in itertools.groupby(sorted_letters):
             letter_counts[key] = len(list(group))
-            
-        print(letter_counts)
 
 
+        self.assertEqual(1, solution(A))
+
+    
 
 
 if __name__ == '__main__':
