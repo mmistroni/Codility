933df3411fa4498d3f785f52038d47e178d74a07
mmistroni <mmistroni@gmail.com>
2025-09-24 19:48:42 +0000

    sybc

--- test/test_trump_detector.py
+++ test/test_trump_detector.py
@@ -1,6 +1,24 @@
 import unittest
 
-from src.trumpness_detector import solution
+from src.trumpness_detector import trump_detector
 
 class MyTestCase(unittest.TestCase):
-    pass
+
+    def test_one(self):
+        self.assertEqual(0, trump_detector("I will build a huge wall"))
+    
+    def test_two(self):
+
+        self.assertEqual(4, trump_detector("HUUUUUGEEEE WAAAAAALL"))
+    def test_three(self):
+        self.assertEquals( 2.5, trump_detector("MEXICAAAAAAAANS GOOOO HOOOMEEEE"))
+    def test_four(self):
+        self.assertEquals(1.89, trump_detector("America NUUUUUKEEEE Oooobaaaamaaaaa"))
+    def test_five(self):
+        self.assertEquals(1.56, 
+                          trump_detector("listen migrants: IIII KIIIDD YOOOUUU NOOOOOOTTT"))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
