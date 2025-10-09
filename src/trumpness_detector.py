3d00d2ce96a26ba84decaf415ea1992bb86d225d
mmistroni <mmistroni@gmail.com>
2025-10-09 20:14:13 +0000

    minor change

--- src/trumpness_detector.py
+++ src/trumpness_detector.py
@@ -1,4 +1,19 @@
 #https://www.codewars.com/kata/57829376a1b8d576640000d6/train/python
+import itertools
 
-def trump_detector(w):
-    return 0
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+def group_consecutive_letters_itertools(sentence: str) -> list:
+    """
+    Groups consecutive identical letters using itertools.groupby.
+    """
+    return [(key, len(list(group))) for key, group in itertools.groupby(sentence)]        
+
+
+def trump_detector(sentence):
+    grouped_letters = group_consecutive_letters_itertools(sentence)
+    sampled = [t for t in grouped_letters if t[0].lower() in vowels]
+    reduced = [t[1]-1 for t in sampled]
+    numv = [t[0] for t in sampled]
+
+    return round(sum(reduced) / len(numv), 2)
--- test/test_trump_detector.py
+++ test/test_trump_detector.py
@@ -5,19 +5,19 @@ from src.trumpness_detector import trump_detector
 class MyTestCase(unittest.TestCase):
     
     def test_one(self):
-        self.assertEquals(trump_detector("I will build a huge wall"), 0)
+        self.assertEqual(trump_detector("I will build a huge wall"), 0)
         
     def test_two(self):
-        self.assertEquals(4, trump_detector("HUUUUUGEEEE WAAAAAALL"))
+        self.assertEqual(4, trump_detector("HUUUUUGEEEE WAAAAAALL"))
     
     def test_three(self):
-        self.assertEquals(2.5, trump_detector("MEXICAAAAAAAANS GOOOO HOOOMEEEE"))
+        self.assertEqual(2.5, trump_detector("MEXICAAAAAAAANS GOOOO HOOOMEEEE"))
 
     def test_four(self):
-        self.assertEquals(1.89, trump_detector("America NUUUUUKEEEE Oooobaaaamaaaaa"))
+        self.assertEqual(1.89, trump_detector("America NUUUUUKEEEE Oooobaaaamaaaaa"))
 
     def test_five(self):
-        self.assertEquals(1.56, 
+        self.assertEqual(1.56, 
                 trump_detector("listen migrants: IIII KIIIDD YOOOUUU NOOOOOOTTT"))
 
 
@@ -31,13 +31,13 @@ class MyTestCase(unittest.TestCase):
             return [(key, len(list(group))) for key, group in itertools.groupby(sentence)]        
 
 
-        sentence = "IIII KIIIDD YOOOUUU NOOOOOOTTT" #
+        sentence = "listen migrants:IIII KIIIDD YOOOUUU NOOOOOOTTT" #
         # --> 14 extra vowels on 9 base ones give 1.55555555... which is rounded to 1.56
         #"HUUUUUGEEEE WAAAAAALL"
 
         vowels = ['a', 'e', 'i', 'o', 'u']
 
-        my_sentence = "IIII KIIIDD YOOOUUU NOOOOOOTTT"
+        my_sentence = sentence #"IIII KIIIDD YOOOUUU NOOOOOOTTT"
         grouped_letters = group_consecutive_letters_itertools(my_sentence)
 
         sampled = [t for t in grouped_letters if t[0].lower() in vowels]
@@ -48,6 +48,11 @@ class MyTestCase(unittest.TestCase):
         numv = [t[0] for t in sampled]
 
         print(f'NV:{numv}={len(numv)}')
+        print(f'reduced:{sum(reduced)}')
+
+        answer = round(sum(reduced) / len(numv), 2)
+
+        print(f'REsult:{answer}')
 
 
     
