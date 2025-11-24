04f859195dfac71a346ae6c8048ffef77c627b9c
mmistroni <mmistroni@gmail.com>
2025-11-24 21:19:37 +0000

    improved

--- src/without_two_zeros.py
+++ src/without_two_zeros.py
@@ -13,18 +13,29 @@ For example, there are three eligible binary numbers of length 3:
 
 Remember that n may be fairly big (up to 10^4). Good luck!
 
+
 '''
 
 import itertools
 
 symbols = ['0', '1']
 
-def _generate_combi(length):
-    combinations_tuples = list(itertools.product(symbols, repeat=length))
+def _generate_combi(length_n):
+    #combinations_tuples = list(itertools.product(symbols, repeat=length))
     # Join the elements of each tuple into a single string for cleaner output
-    return [''.join(combo) for combo in combinations_tuples]
+    #return [''.join(combo) for combo in combinations_tuples]
+    total_combinations = 2**length_n  # This is 8
 
+    binary_list = []
+    for i in range(total_combinations):
+        # 1. Convert the integer 'i' to its binary string: bin(i)
+        #    Example: i=1 -> '0b1', i=4 -> '0b100'
+        # 2. Remove the '0b' prefix: [2:]
+        # 3. Use zfill(length_n) to pad with leading zeros up to the required length (n)
+        binary_string = bin(i)[2:].zfill(length_n)
+        binary_list.append(binary_string)
 
+    return binary_list
 
 def zeros(n: int) -> int:
     combis = _generate_combi(n)
