7381f63ff1b3207dbf3bc72c6cae22ba230ea30b
mmistroni <mmistroni@gmail.com>
2026-01-07 21:07:53 +0000

    commit

--- src/vowel_recognition.py
+++ src/vowel_recognition.py
@@ -1,4 +1,10 @@
 #https://www.codewars.com/kata/5bed96b9a68c19d394000b1d
 
+vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+
+
 def vowel_recognition(s):
+    sts = set(s)
+    if not vowels.intersection(sts):
+        return 0
     # your code here
