8ae7c06e2c300365776d331074c5aff43b012340
mmistroni <mmistroni@gmail.com>
2026-07-27 19:11:53 +0000

    starting a new test

---
+++ src/are_the_same.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/550498447451fbbd7600041c/train/python
+'''
+Given two arrays a and b write a function comp(a, b) (orcompSame(a, b))
+that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+'''
+
+def comp(array1, array2):
+    return False
+
---
+++ test/test_are_the_same.py
@@ -0,0 +1,23 @@
+import unittest
+
+from src.are_the_same import comp
+
+class MyTestCase(unittest.TestCase):
+    def test_one(self):
+        a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+        a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+        self.assertEqual(comp(a1, a2), True)
+
+    def test_two(self):
+        a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+        a2 = [11*21, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+        self.assertEqual(comp(a1, a2), False)
+
+    def test_three(self):
+        a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+        a2 = [11*11, 121*121, 144*144, 190*190, 161*161, 19*19, 144*144, 19*19]
+
+
+
+if __name__ == '__main__':
+    unittest.main()
