c516721911aaf19079e1390d6844bd0514d12ef2
mmistroni <mmistroni@gmail.com>
2026-09-17 20:46:48 +0000

    starting the space cypther

--- src/space_snail_cipher.py
+++ src/space_snail_cipher.py
@@ -8,7 +8,14 @@ def decode(cipher : str ,key : int) -> str:
     return ''
 
 
-def get_directionI(key : int) -> int:
+def get_direction(key : str) -> int:
+    # 1st digit orientation
+    # 2nd digit gap
+    '''
+    Positive key $\rightarrow$ fill center-out (character 0 goes to the spiral center).
+    Negative key $\rightarrow$ fill outside-in (character 0 starts at the outer tip of the spiral).
+    
+    '''
     if key > 0:
         return 1
     else:
